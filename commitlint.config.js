86c818e8e35fa9d012969f1fa61b45921b9d2ed1
Samarpan <samarpan.bhattacharya@sourcefuse.com>
2020-05-31 23:11:00 +0530

    chore: add commitlint, commitizen and jira id enforcer to commit checks
    re #SFO-20

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-lerna-scopes',
+  ],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [0, 'always'],
+    'references-empty': [2, 'never'],
+  },
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ['SFO-'],
+    },
+  },
+};
