eadc6d2ea3695371298b7d223d57e7aaee4bbe55
Bhavya Dhiman <bhavya.dhiman@sourccefuse.com>
2020-06-03 14:01:29 +0530

    feat(video-conferencing-service): Implement Vonage API in Vonage Provider, add authorization and authentciation
    SFO-51: Implement Vonage APIs in Provider. Add authorization and
    authentication strategy in controllers.
    Slight modification in Interfaces and function arguments in
    functions defined in Interface and Vonage Provider.
    
    BREAKING CHANGE: Breaking change may occur if the environment variables are incorrectly defined

--- services/video-conferencing-service/src/controllers/video-chat-session.controller.ts
+++ services/video-conferencing-service/src/controllers/video-chat-session.controller.ts
@@ -7,13 +7,14 @@ import {
   MeetingOptions,
   SessionOptions,
   VideoChatInterface,
-  SessionResponse,
 } from '../types';
 import {VideoChatSessionRepository} from '../repositories/video-chat-session.repository';
 import {VideoChatBindings} from '../keys';
 import { authenticate, STRATEGY } from 'loopback4-authentication';
 import { PermissionKeys } from '../enums/permission-keys.enum';
+import {SessionResponse} from './../types';
 
+import cryptoRandomString = require('crypto-random-string');
 export class VideoChatSessionController {
   constructor(
     @repository(VideoChatSessionRepository)
@@ -61,9 +62,8 @@ export class VideoChatSessionController {
     sessionOptions: SessionOptions,
     @param.path.string('meetingLink') meetingLink: string,
   ): Promise<SessionResponse> {
-    return {sessionId: 'session_one', token: 'secret_token'};
+    return {sessionId: 'session_one', token: 'secret_token'};	
   }
-
   @authenticate(STRATEGY.BEARER)
   @authorize([PermissionKeys.StopMeeting])
   @patch('/session/{meetingLink}/end', {
