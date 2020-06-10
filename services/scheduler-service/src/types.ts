f52e13f5545d79547a9822a01df2c08e68763103
vineet-suri <vineet.suri@sourcefuse.com>
2020-06-10 16:10:25 +0530

    docs(scheduler-service): update readme
    SFO-78

--- services/scheduler-service/src/types.ts
+++ services/scheduler-service/src/types.ts
@@ -1,9 +1,3 @@
-import { IAuthUserWithPermissions } from '@sourcefuse-service-catalog/core';
-
-export interface CustomPermissionFn {
-    (user: IAuthUserWithPermissions): Promise<boolean>;
-  }
-
 export interface ISchedulerConfig {
   jwtIssuer: string;
   jwtSecret: string;
