92febbce5ccc409e04400281a2b3a585f48512b7
Rashi <rashi.sharma@sourcefuse.com>
2020-07-07 17:24:32 +0530

    feat(scheduler-service): change trigger name
    SFO-106

--- services/scheduler-service/migrations/sqls/20200707111059-add-modifiedOn-triggers-down.sql
+++ services/scheduler-service/migrations/sqls/20200707111059-add-modifiedOn-triggers-down.sql
@@ -3,7 +3,7 @@
 SET search_path TO logs, public;
 
 DROP TRIGGER mdt_calendars ON scheduler.calendars;
-DROP TRIGGER mdt_subscriptiONs ON scheduler.subscriptions;
+DROP TRIGGER mdt_subscriptions ON scheduler.subscriptions;
 DROP TRIGGER mdt_working_hours ON scheduler.working_hours;
 DROP TRIGGER mdt_events ON scheduler.events;
 DROP TRIGGER mdt_attendees ON scheduler.attendees;
