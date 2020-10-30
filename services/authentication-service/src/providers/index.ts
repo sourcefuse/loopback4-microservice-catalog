8f37477f758b917bb7ac53021966491087536c2d
ishu-sf <59956047+IshuSF@users.noreply.github.com>
2020-10-31 00:30:47 +0530

    feat(authentication-service): added keycloak signup provider (#89)
    * feat(authentication-service): added keycloak signup provider
    
    added keycloak signup provider
    
    * style(authentication-service): sonar fix
    
    sonar fix

--- services/authentication-service/src/providers/index.ts
+++ services/authentication-service/src/providers/index.ts
@@ -1,3 +1,4 @@
 export * from './keys';
 export * from './types';
 export * from './google-oauth2-signup.provider';
+export * from './keycloak-signup.provider';
--- services/authentication-service/src/providers/keys.ts
+++ services/authentication-service/src/providers/keys.ts
@@ -1,8 +1,14 @@
 import {BindingKey} from '@loopback/core';
-import {GoogleSignUpFn} from './types';
+import {GoogleSignUpFn, KeyCloakSignUpFn} from './types';
 
 export namespace SignUpBindings {
   export const GOOGLE_SIGN_UP_PROVIDER = BindingKey.create<GoogleSignUpFn>(
     'sf.google.signup.provider',
+  );
+}
+
+export namespace KeyCloakSignUpBindings {
+  export const KEYCLOAK_SIGN_UP_PROVIDER = BindingKey.create<KeyCloakSignUpFn>(
+    'sf.keycloak.signup.provider',
   );
 }
