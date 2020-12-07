8d27e0beef37033a4b4f3c6424fcf140c6c6d877
vineet-suri <vineet.suri@sourcefuse.com>
2020-12-07 16:32:47 +0530

    feat(notification-service): adds dynamic properties to notification user model
    RFIT-1121

--- services/notification-service/src/models/notification-user.model.ts
+++ services/notification-service/src/models/notification-user.model.ts
@@ -43,6 +43,13 @@ export class NotificationUser extends BaseEntity {
   })
   actionMeta?: AnyObject;
 
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+
   constructor(data?: Partial<NotificationUser>) {
     super(data);
   }
