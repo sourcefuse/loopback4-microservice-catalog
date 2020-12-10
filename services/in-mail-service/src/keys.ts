51e19de4f392aa4a70e621559997cbba1366ddc5
Akash Kansara <akash.kansara@sourcefuse.com>
2020-12-10 15:14:27 +0530

    add / use 'useCustomSequence' option

--- services/in-mail-service/src/keys.ts
+++ services/in-mail-service/src/keys.ts
@@ -1,8 +1,9 @@
 import {BindingKey} from '@loopback/core';
-import {BINDING_PREFIX, IServiceConfig} from '@sourceloop/core';
+import {BINDING_PREFIX} from '@sourceloop/core';
+import {IInMailServiceConfig} from './types';
 
 export namespace InMailBindings {
-  export const Config = BindingKey.create<IServiceConfig | null>(
+  export const Config = BindingKey.create<IInMailServiceConfig | null>(
     `${BINDING_PREFIX}.inmail.config`,
   );
 }
--- services/in-mail-service/src/types/index.ts
+++ services/in-mail-service/src/types/index.ts
@@ -3,3 +3,4 @@ export * from './party-marker.type';
 export * from './permissions.type';
 export * from './storage-marker.type';
 export * from './visible-marker.type';
+export * from './service-config.type';
