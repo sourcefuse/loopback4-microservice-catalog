9b0934305e1522c93d1b3a8093488765921830f9
prernagp <prerna.gupta@sourcefuse.com>
2020-12-22 16:48:58 +0530

    feat(Transactional Repo): Added base repo

--- packages/core/src/repositories/index.ts
+++ packages/core/src/repositories/index.ts
@@ -1,2 +1,3 @@
 export * from './default-soft-crud.repository.base';
 export * from './default-user-modify-crud.repository.base';
+export * from './default-transactional-user-modify-repository.base';
