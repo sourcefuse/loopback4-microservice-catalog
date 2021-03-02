c11411a9c8995017fa5181785e61e7c10aa3249c
Akshat Dubey <akshat.dubey@sourcefuse.com>
2021-03-02 15:20:08 +0530

    Fix: fixed linting

--- services/authentication-service/src/types.ts
+++ services/authentication-service/src/types.ts
@@ -11,6 +11,6 @@ export interface PreSignupFn<T = AnyObject, S = AnyObject> {
   (request: SignupRequestDto<T>): Promise<S>;
 }
 
-export interface UserSignupFn<T, S=AnyObject> {
+export interface UserSignupFn<T, S = AnyObject> {
   (model: T): Promise<S>;
 }
