d917a55e2776dfcf7fce103183f9f7f51007fef4
Akshat Dubey <akshat.dubey@sourcefuse.com>
2021-03-02 14:55:59 +0530

    Fix: fixed presignup function type

--- services/authentication-service/src/types.ts
+++ services/authentication-service/src/types.ts
@@ -1,3 +1,4 @@
+import { AnyObject } from '@loopback/repository';
 import {IServiceConfig} from '@sourceloop/core';
 import {SignupRequestDto} from './models/signup-request-dto.model';
 
@@ -6,8 +7,8 @@ export interface IAuthServiceConfig extends IServiceConfig {}
 export const AuthDbSourceName = 'AuthDB';
 export const AuthCacheSourceName = 'AuthCache';
 
-export interface PreSignupFn<T = void> {
-  (request: SignupRequestDto): Promise<T>;
+export interface PreSignupFn<T = AnyObject> {
+  (request: SignupRequestDto<T>): Promise<T>;
 }
 
 export interface UserSignupFn<T> {
