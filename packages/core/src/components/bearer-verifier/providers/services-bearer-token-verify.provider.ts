26a67c5b2c4770ba2a464aba20b198a22c151637
Akshat Dubey <akshat.dubey@sourcefuse.com>
2021-03-27 13:54:34 +0530

    fix(core): undo change in core
    RPMS-0

--- packages/core/src/components/bearer-verifier/providers/services-bearer-token-verify.provider.ts
+++ packages/core/src/components/bearer-verifier/providers/services-bearer-token-verify.provider.ts
@@ -19,7 +19,6 @@ export class ServicesBearerTokenVerifyProvider
       try {
         user = verify(token, process.env.JWT_SECRET as string, {
           issuer: process.env.JWT_ISSUER,
-          algorithms: ['HS256']
         }) as IAuthUserWithPermissions;
       } catch (error) {
         this.logger.error(JSON.stringify(error));
