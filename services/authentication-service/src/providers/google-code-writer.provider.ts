abbbef1a0e84d2f8ffcfe4f8eea4806298cd0208
Akshat Dubey <akshat.dubey@sourcefuse.com>
2021-03-27 12:41:46 +0530

    refactor(authentication-service): fixed linting issues
    RPMS-0

--- services/authentication-service/src/providers/google-code-writer.provider.ts
+++ services/authentication-service/src/providers/google-code-writer.provider.ts
@@ -2,7 +2,6 @@ import {Provider} from '@loopback/core';
 import {GoogleCodeWriterFn} from './types';
 
 export class GoogleCodeWriterProvider implements Provider<GoogleCodeWriterFn> {
-
   value(): GoogleCodeWriterFn {
     return async token => token;
   }
--- services/authentication-service/src/providers/keycloak-code-writer.provider.ts
+++ services/authentication-service/src/providers/keycloak-code-writer.provider.ts
@@ -2,7 +2,6 @@ import {Provider} from '@loopback/core';
 import {KeyCloakCodeWriterFn} from './types';
 
 export class KeyCloakWriterProvider implements Provider<KeyCloakCodeWriterFn> {
-
   value(): KeyCloakCodeWriterFn {
     return async token => token;
   }
