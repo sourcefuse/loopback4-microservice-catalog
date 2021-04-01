cbeb8f24f4fe04a8ecdb6e5f2cb8a3b4d9354c24
Akshat Dubey <akshat.dubey@sourcefuse.com>
2021-04-01 14:25:37 +0530

    refactor(authentication-service): fixed sonar code smell in index of custom-sf-changelog
    gh-124

--- packages/custom-sf-changelog/index.js
+++ packages/custom-sf-changelog/index.js
@@ -1,9 +1,9 @@
 'use strict'
 const Q = require('q')
-const conventionalChangelog = require('./conventional-changelog')
-const parserOpts = require('./parser-opts')
-const recommendedBumpOpts = require('./conventional-recommended-bump')
-const writerOpts = require('./writer-opts')
+const conventionalChangelogOuter = require('./conventional-changelog')
+const parserOptsOuter = require('./parser-opts')
+const recommendedBumpOptsOuter = require('./conventional-recommended-bump')
+const writerOptsOuter = require('./writer-opts')
 
 module.exports = Q.all([conventionalChangelogOuter, parserOptsOuter, recommendedBumpOptsOuter, writerOptsOuter])
   .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
