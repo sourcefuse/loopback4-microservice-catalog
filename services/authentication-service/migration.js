cfebd0cd569695fbf8aa0c125e1d1bf8df32b57e
Akshat Dubey <akshat.dubey@sourcefuse.com>
2021-04-17 10:50:21 +0530

    fix(authentication-service): fixed minor issues in migration.js

--- services/authentication-service/migration.js
+++ services/authentication-service/migration.js
@@ -2,7 +2,10 @@ const dotenv = require('dotenv');
 const dotenvExt = require('dotenv-extended');
 const fs = require('fs');
 const DBMigrate = require('db-migrate');
+const path = require('path');
 let isLocal = false;
+dotenv.config({path: `${process.env.INIT_CWD}/.env`});
+
 try {
   if (fs.existsSync('.infolder')) {
     isLocal = true;
@@ -10,10 +13,13 @@ try {
 } catch (err) {
   console.info('\n');
 }
-if (isLocal || process.env.AUTH_MIGRATION_SKIP) {
+if (
+  isLocal ||
+  process.env.WORKFLOW_MIGRATION_SKIP ||
+  process.env.SOURCELOOP_MIGRATION_SKIP
+) {
   console.info(`Skipping migrations`);
 } else {
-  dotenv.config();
   dotenvExt.load({
     schema: `./migrations/.env.schema`,
     path: `${process.env.INIT_CWD}/.env`,
@@ -22,4 +28,48 @@ if (isLocal || process.env.AUTH_MIGRATION_SKIP) {
   });
   const dbmigrate = DBMigrate.getInstance(true);
   dbmigrate.up();
+}
+
+if (
+  process.env.SOURCELOOP_MIGRATION_COPY ||
+  process.env.WORKFLOW_MIGRATION_COPY
+) {
+  copyFileSync('./database.json', process.env.INIT_CWD);
+  copyFolderRecursiveSync('./migrations', process.env.INIT_CWD);
+}
+
+function copyFileSync(source, target) {
+  var targetFile = target;
+
+  // If target is a directory, a new file with the same name will be created
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source, target) {
+  var files = [];
+
+  // Check if folder needs to be created or integrated
+  var targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  // Copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach(function (file) {
+      var curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
 }
