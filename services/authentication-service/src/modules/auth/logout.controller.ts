69867579763f52135c2fa85f054dee5cecaa9140
Samarpan <samarpan.bhattacharya@sourcefuse.com>
2021-04-26 18:32:24 +0530

    fix(authentication-service): allow keycloak logout on logout
    RPMS-4791

--- services/authentication-service/src/modules/auth/logout.controller.ts
+++ services/authentication-service/src/modules/auth/logout.controller.ts
@@ -13,12 +13,17 @@ import {
   AuthenticateErrorKeys,
   CONTENT_TYPE,
   ErrorCodes,
+  ILogger,
+  LOGGER,
   OPERATION_SECURITY_SPEC,
   STATUS_CODE,
   SuccessResponse,
 } from '@sourceloop/core';
+import {encode} from 'base-64';
 import {authenticate, AuthErrorKeys, STRATEGY} from 'loopback4-authentication';
 import {authorize} from 'loopback4-authorization';
+import * as fetch from 'node-fetch';
+import {URLSearchParams} from 'url';
 
 import {RefreshTokenRequest} from '../../models';
 import {
@@ -33,6 +38,7 @@ export class LogoutController {
     private readonly revokedTokens: RevokedTokenRepository,
     @repository(RefreshTokenRepository)
     public refreshTokenRepo: RefreshTokenRepository,
+    @inject(LOGGER.LOGGER_INJECT) public logger: ILogger,
   ) {}
 
   @authenticate(STRATEGY.BEARER, {
@@ -82,6 +88,103 @@ export class LogoutController {
     if (!refreshTokenModel) {
       throw new HttpErrors.Unauthorized(AuthErrorKeys.TokenExpired);
     }
+    if (refreshTokenModel.accessToken !== token) {
+      throw new HttpErrors.Unauthorized(AuthErrorKeys.TokenInvalid);
+    }
+    await this.revokedTokens.set(token, {token});
+    await this.refreshTokenRepo.delete(req.refreshToken);
+    if (refreshTokenModel.pubnubToken) {
+      await this.refreshTokenRepo.delete(refreshTokenModel.pubnubToken);
+    }
+
+    return new SuccessResponse({
+      success: true,
+
+      key: refreshTokenModel.userId,
+    });
+  }
+
+  @authenticate(STRATEGY.BEARER, {
+    passReqToCallback: true,
+  })
+  @authorize({permissions: ['*']})
+  @post('/keycloak/logout', {
+    security: OPERATION_SECURITY_SPEC,
+    description:
+      'This API will log out the user from application as well as keycloak',
+    responses: {
+      [STATUS_CODE.OK]: {
+        description: 'Success Response',
+        content: {
+          [CONTENT_TYPE.JSON]: {
+            schema: {'x-ts-type': SuccessResponse},
+          },
+        },
+      },
+      ...ErrorCodes,
+    },
+  })
+  async keycloakLogout(
+    @param.header.string('Authorization', {
+      description:
+        'This is the access token which is required to authenticate user.',
+    })
+    auth: string,
+    @requestBody({
+      content: {
+        [CONTENT_TYPE.JSON]: {
+          schema: getModelSchemaRef(RefreshTokenRequest, {
+            partial: true,
+          }),
+        },
+      },
+    })
+    req: RefreshTokenRequest,
+  ): Promise<SuccessResponse> {
+    const token = auth?.replace(/bearer /i, '');
+    if (!token || !req.refreshToken) {
+      throw new HttpErrors.UnprocessableEntity(
+        AuthenticateErrorKeys.TokenMissing,
+      );
+    }
+
+    const refreshTokenModel = await this.refreshTokenRepo.get(req.refreshToken);
+    if (!refreshTokenModel) {
+      throw new HttpErrors.Unauthorized(AuthErrorKeys.TokenExpired);
+    }
+
+    if (refreshTokenModel.externalRefreshToken) {
+      const params = new URLSearchParams();
+      const logoutUrl = `${process.env.KEYCLOAK_HOST}/auth/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/logout`;
+      params.append('refresh_token', refreshTokenModel.externalRefreshToken);
+      fetch
+        .default(logoutUrl, {
+          method: 'post',
+          body: params,
+          headers: {
+            Authorization:
+              'Basic ' +
+              encode(
+                process.env.KEYCLOAK_CLIENT_ID +
+                  ':' +
+                  process.env.KEYCLOAK_CLIENT_SECRET,
+              ),
+          },
+        })
+        .then(() => {
+          this.logger.info(
+            `User ${refreshTokenModel.username} logged off successfully from keycloak.`,
+          );
+        })
+        .catch(err => {
+          this.logger.error(
+            `Error while logging off from keycloak. Error :: ${err} ${JSON.stringify(
+              err,
+            )}`,
+          );
+        });
+    }
+
     if (refreshTokenModel.accessToken !== token) {
       throw new HttpErrors.Unauthorized(AuthErrorKeys.TokenInvalid);
     }
