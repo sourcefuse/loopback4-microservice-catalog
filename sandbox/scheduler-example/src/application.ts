171855349b0667454659067b425b7d2ca76f1827
unknown <sumiter92@gmail.com>
2021-05-04 00:44:55 +0530

    feat:scheduler-example

---
+++ sandbox/scheduler-example/src/application.ts
@@ -0,0 +1,66 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {ServiceMixin} from '@loopback/service-proxy';
+import {SchedulerServiceComponent} from '@sourceloop/scheduler-service';
+import * as dotenv from 'dotenv';
+import * as dotenvExt from 'dotenv-extended';
+import path from 'path';
+import {MySequence} from './sequence';
+
+export {ApplicationConfig};
+
+const port = 3000;
+export class SchedulerExampleApplication extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    dotenv.config();
+    if (process?.env?.NODE_ENV && process.env.NODE_ENV !== 'test') {
+      dotenvExt.load({
+        schema: '.env.example',
+        errorOnMissing: true,
+        includeProcessEnv: true,
+      });
+    } else {
+      dotenvExt.load({
+        schema: '.env.example',
+        errorOnMissing: false,
+        includeProcessEnv: true,
+      });
+    }
+    options.rest = options.rest || {};
+    options.rest.port = +(process.env.PORT ?? port);
+    options.rest.host = process.env.HOST;
+    super(options);
+
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+    this.component(SchedulerServiceComponent);
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+  }
+}
