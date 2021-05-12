98ef52919a8a165266576927b96167574c46b270
vaibhavmauryasf <vaibhav.maurya@sourcefuse.com>
2021-05-12 17:03:10 +0530

    Update tour-store-service.service.ts

--- packages/user-onboarding/projects/user-onboarding-lib/src/lib/tour-store-service.service.ts
+++ packages/user-onboarding/projects/user-onboarding-lib/src/lib/tour-store-service.service.ts
@@ -55,4 +55,24 @@ export class TourStoreServiceService {
   public getFnByKey(key){
     return this.functionMap.get(key);
   }
+
+  // public registerIDGenerator(key){
+  //   this.functionMap.set(key,this.create_UUID);
+  // }
+
+  public create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+  }
+
+  public loadID(){
+      const UUID = this.create_UUID();
+      return UUID;
+  }
+
 }
