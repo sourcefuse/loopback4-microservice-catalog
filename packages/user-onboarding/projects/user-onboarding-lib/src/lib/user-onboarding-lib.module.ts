527eb0081bd0ac110cfa81f2d8d75dc304673866
vaibhavmauryasf <vaibhav.maurya@sourcefuse.com>
2021-05-25 11:54:34 +0530

    fix(user-onboarding): code smell fix
    fixed some code smells in the code

--- packages/user-onboarding/projects/user-onboarding-lib/src/lib/user-onboarding-lib.module.ts
+++ packages/user-onboarding/projects/user-onboarding-lib/src/lib/user-onboarding-lib.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-import { UserOnboardingLibComponent } from './user-onboarding-lib.component';
+
 import { StorageServiceModule } from 'ngx-webstorage-service';
 
 
 @NgModule({
   declarations: [
-    UserOnboardingLibComponent
+
   ],
   imports: [
     StorageServiceModule
   ],
   exports: [
-    UserOnboardingLibComponent
   ]
 })
 export class UserOnboardingLibModule { }
