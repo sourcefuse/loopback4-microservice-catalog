e4d09c28bc7897cc5792ab7e4fc2c770787cb592
vaibhavmauryasf <vaibhav.maurya@sourcefuse.com>
2021-06-08 17:44:10 +0530

    fix(user-onboarding): issue fix
    fixed issues in lerna test

--- packages/user-onboarding/projects/user-onboarding-lib/src/test.ts
+++ packages/user-onboarding/projects/user-onboarding-lib/src/test.ts
@@ -1,29 +1,14 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone';
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-import 'zone.js/dist/zone-testing';
-
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
+  platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-(window as any).global = window;
-
 declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp
-  ): {
+  context(path: string, deep?: boolean, filter?: RegExp): {
     keys(): string[];
     <T>(id: string): T;
   };
@@ -37,4 +22,4 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-context.keys().forEach(context);
+context.keys().map(context);
