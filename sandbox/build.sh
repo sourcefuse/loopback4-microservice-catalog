31134f5e781fd2957db31b52c905649428987023
jamescrowley321 <jamescrowley151@gmail.com>
2021-07-28 11:23:36 -0500

    chore(sandbox): workaround-build-agent
    Workaround for build agent and sudo issue
    
    gh-261

--- sandbox/build.sh
+++ sandbox/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 REGISTRY=$1
 
+# TODO: remove sudo once build agent is fixed
+
 if [ -z "$REGISTRY" ]; then
   REGISTRY="localhost:32000"
   echo "${REGISTRY}"
@@ -10,14 +12,14 @@ fi
 
 if [ ! -z "${DOCKER_USERNAME}" ] && [ ! -z "${DOCKER_PASSWORD}" ]; then
   echo "Logging in to Docker"
-  echo "${DOCKER_PASSWORD}" | docker login --username ${DOCKER_USERNAME} --password-stdin
+  echo "${DOCKER_PASSWORD}" | sudo docker login --username ${DOCKER_USERNAME} --password-stdin
 fi
 
-export REGISTRY=$REGISTRY; docker-compose build
-docker push ${REGISTRY}/auth-multitenant-example
-docker push ${REGISTRY}/notification-socket-example
-docker push ${REGISTRY}/workflow-ms-example
-docker push ${REGISTRY}/audit-ms-example
-docker push ${REGISTRY}/scheduler-example
-docker push ${REGISTRY}/video-conferencing-ms-example
-docker push ${REGISTRY}/in-mail-example
+export REGISTRY=$REGISTRY; sudo docker-compose build
+sudo docker push ${REGISTRY}/auth-multitenant-example
+sudo docker push ${REGISTRY}/notification-socket-example
+sudo docker push ${REGISTRY}/workflow-ms-example
+sudo docker push ${REGISTRY}/audit-ms-example
+sudo ocker push ${REGISTRY}/scheduler-example
+sudo docker push ${REGISTRY}/video-conferencing-ms-example
+sudo docker push ${REGISTRY}/in-mail-example
