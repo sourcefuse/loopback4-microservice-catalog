ab00405c74b706cbd3946702bfee110d67a8d3ac
jamescrowley321 <jamescrowley151@gmail.com>
2021-07-29 17:18:39 -0500

    chore(sandbox): workaround-build-agent
    workaround-build-agent
    
    gh-261

--- sandbox/ci_build.sh
+++ sandbox/ci_build.sh
@@ -15,7 +15,7 @@ echo "CURRENT_DIR=$CURRENT_DIR"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o ${CURRENT_DIR}/docker-compose --insecure
 sudo chmod +x ${CURRENT_DIR}/docker-compose
 
-export REGISTRY=$DOCKER_USERNAME;sudo ./docker-compose -f "${CURRENT_DIR}/docker-compose.yml" build
+sudo REGISTRY=$DOCKER_USERNAME ./docker-compose -f "${CURRENT_DIR}/docker-compose.yml" build
 sudo docker push ${DOCKER_USERNAME}/auth-multitenant-example
 sudo docker push ${DOCKER_USERNAME}/notification-socket-example
 sudo docker push ${DOCKER_USERNAME}/workflow-ms-example
@@ -23,3 +23,4 @@ sudo docker push ${DOCKER_USERNAME}/audit-ms-example
 sudo docker push ${DOCKER_USERNAME}/scheduler-example
 sudo docker push ${DOCKER_USERNAME}/video-conferencing-ms-example
 sudo docker push ${DOCKER_USERNAME}/in-mail-example
+sudo docker system prune -a -f
