d7dbf5f0d848674f46ffadac0492d0d9dedfcdb5
jamescrowley321 <jamescrowley151@gmail.com>
2021-07-29 16:34:30 -0500

    chore(sandbox): fix-jenkinsfile
    fix-jenkinsfile
    
    gh-261

--- sandbox/ci_build.sh
+++ sandbox/ci_build.sh
@@ -12,6 +12,9 @@ fi
 
 echo "CURRENT_DIR=$CURRENT_DIR"
 
+sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)\" -o ${CURRENT_DIR}/sandbox/docker-compose
+sudo chmod +x ${CURRENT_DIR}/sandbox/docker-compose
+
 export REGISTRY=$DOCKER_USERNAME; ./docker-compose -f "${CURRENT_DIR}/docker-compose.yml" build
 sudo docker push ${DOCKER_USERNAME}/auth-multitenant-example
 sudo docker push ${DOCKER_USERNAME}/notification-socket-example
