c58de2207c5a7bd41abd5f7f2ddba8737f33e0f4
Ashutosh Bansal <ashutosh.bansal@sourcefuse.com>
2021-07-30 17:31:03 +0530

    fix(sandbox): naming convention correction

--- sandbox/casbin-example/src/modules/auth/models/client-auth-request.dto.ts
+++ sandbox/casbin-example/src/modules/auth/models/client-auth-request.dto.ts
@@ -1,5 +1,3 @@
-/* eslint-disable  @typescript-eslint/naming-convention */
-
 import {Model, model, property} from '@loopback/repository';
 import {ModelPropertyDescriptionString} from './model-property-description.enum';
 
