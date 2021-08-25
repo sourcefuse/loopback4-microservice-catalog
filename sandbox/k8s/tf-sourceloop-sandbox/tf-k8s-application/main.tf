4259b7b5f4ec06f9bc9b43bea20ea437e0e99e74
Travis Saucier <travis.saucier@sourcefuse.com>
2021-08-25 11:45:22 -0400

    add new persistent volume claim resource to the module. added new variables.

--- sandbox/k8s/tf-sourceloop-sandbox/tf-k8s-application/main.tf
+++ sandbox/k8s/tf-sourceloop-sandbox/tf-k8s-application/main.tf
@@ -1,9 +1,9 @@
 
 resource "kubernetes_service" "default" {
-
   metadata {
     name      = var.service_name
     namespace = var.namespace_name
+
     labels = {
       app : var.app_label
     }
@@ -16,6 +16,7 @@ resource "kubernetes_service" "default" {
       target_port = var.target_port
       protocol    = var.protocol
     }
+
     selector = {
       app : var.app_label
     }
@@ -26,12 +27,14 @@ resource "kubernetes_deployment" "default" {
   metadata {
     name      = var.deployment_name
     namespace = var.namespace_name
+
     labels = {
       app : var.app_label
     }
   }
   spec {
     replicas = var.replica_count
+
     selector {
       match_labels = {
         app : var.app_label
@@ -41,14 +44,17 @@ resource "kubernetes_deployment" "default" {
     template {
       metadata {
         namespace = var.namespace_name
+
         labels = {
           app : var.app_label
         }
       }
+
       spec {
         container {
           name  = var.container_name
           image = var.container_image
+
           port {
             container_port = var.container_port
           }
@@ -56,6 +62,7 @@ resource "kubernetes_deployment" "default" {
           dynamic "env" {
             iterator = environment_variable
             for_each = var.environment_variables
+
             content {
               name  = environment_variable.value["name"]
               value = environment_variable.value["value"]
@@ -67,3 +74,27 @@ resource "kubernetes_deployment" "default" {
   }
 }
 
+## pv and pvc
+resource "kubernetes_persistent_volume_claim" "default" {
+  count = var.persistent_volume_claim_enable == true ? 1 : 0
+
+  metadata {
+    name        = var.persistent_volume_claim_name
+    annotations = var.persistent_volume_claim_annotations
+    labels      = var.persistent_volume_claim_labels
+    namespace   = var.persistent_volume_claim_namespace
+  }
+
+  // TODO - should we add a PV resource to this module as a failback for the volume_name?
+  spec {
+    access_modes       = var.persistent_volume_claim_access_modes
+    selector           = var.persistent_volume_claim_selector
+    volume_name        = try(var.persistent_volume_claim_volume_name, null)
+    storage_class_name = var.persistent_volume_claim_storage_class_name
+
+    resources {
+      limits   = var.persistent_volume_claim_resource_limits
+      requests = var.persistent_volume_claim_resource_request
+    }
+  }
+}
--- sandbox/k8s/tf-sourceloop-sandbox/tf-k8s-application/variables.tf
+++ sandbox/k8s/tf-sourceloop-sandbox/tf-k8s-application/variables.tf
@@ -63,3 +63,76 @@ variable "environment_variables" {
   type        = list(object({ name = string, value = string }))
   default     = []
 }
+
+####################################################
+## persistent volume and claims
+####################################################
+## pvc
+variable "persistent_volume_claim_access_modes" {
+  description = "A set of the desired access modes the volume should have."
+  type        = list(string)
+  default     = ["ReadWriteMany"]
+}
+
+variable "persistent_volume_claim_annotations" {
+  description = "An unstructured key value map stored with the persistent volume claim that may be used to store arbitrary metadata."
+  type        = map(any)
+  default     = {}
+}
+
+variable "persistent_volume_claim_enable" {
+  description = "Enable a persistent volume claim."
+  type        = bool
+  default     = false
+}
+
+variable "persistent_volume_claim_labels" {
+  description = "Map of string keys and values that can be used to organize and categorize (scope and select) the persistent volume claim. May match selectors of replication controllers and services."
+  type        = map(any)
+  default     = {}
+}
+
+variable "persistent_volume_claim_name" {
+  description = "Name of the persistent volume claim, must be unique. Cannot be updated."
+  default     = null
+}
+
+variable "persistent_volume_claim_namespace" {
+  description = "Namespace defines the space within which name of the persistent volume claim must be unique."
+  default     = null
+}
+
+variable "persistent_volume_claim_selector" {
+  description = "A label query over volumes to consider for binding."
+  default     = null
+}
+
+variable "persistent_volume_claim_volume_name" {
+  description = "The binding reference to the PersistentVolume backing this claim."
+  default     = null
+}
+
+variable "persistent_volume_claim_resource_limits" {
+  description = "Map describing the maximum amount of compute resources allowed."
+  type        = map(string)
+  default     = {}
+}
+
+variable "persistent_volume_claim_resource_request" {
+  description = "Map describing the minimum amount of compute resources required."
+  type        = map(string)
+
+  default = {
+    storage = "5Gi"
+  }
+}
+
+variable "persistent_volume_claim_storage_class_name" {
+  description = "Name of the storage class requested by the claim."
+  default     = null
+}
+
+variable "persistent_volume_claim_storage_size" {
+  description = "Map describing the minimum amount of compute resources required."
+  default     = null
+}
