2982cc5ab466d8296f6ba4b0526bfe4b03536418
Ashutosh Bansal <ashutosh.bansal@sourcefuse.com>
2021-09-02 15:47:21 +0530

    fix(in-mail-service): added test cases
    gh-312

--- services/in-mail-service/src/__tests__/unit/originator.controller.unit.ts
+++ services/in-mail-service/src/__tests__/unit/originator.controller.unit.ts
@@ -5,13 +5,14 @@ import {
   StubbedInstanceWithSinonAccessor,
 } from '@loopback/testlab';
 import {OriginatorController} from '../../controllers';
+import {IdArrays, Message, StatusMarker} from '../../models';
 import {
   AttachmentRepository,
   GroupRepository,
   MessageRepository,
   ThreadRepository,
 } from '../../repositories';
-import {StorageMarker} from '../../types';
+import {PartyTypeMarker, StorageMarker, VisibilityMarker} from '../../types';
 import {
   getSampleDataWithOutGroup,
   getSampleMailData,
@@ -21,9 +22,16 @@ import {
   thread,
   transactionStub,
   user,
+  attachment,
 } from './sample-data';
 const sampleMessageId = 'sample-message-id';
 const sampleExtId = 'sample-ext-id';
+const sampleMarkMail = 'read';
+const sampleAttachmentId = 'sample-attachment-id';
+const sampleIdArray = new IdArrays({
+  messageIds: ['sample=message-id'],
+  threadIds: ['sample-thread-id'],
+});
 let messageRepository: StubbedInstanceWithSinonAccessor<MessageRepository>;
 let threadRepository: StubbedInstanceWithSinonAccessor<ThreadRepository>;
 let groupRepository: StubbedInstanceWithSinonAccessor<GroupRepository>;
@@ -67,6 +75,139 @@ describe('originatorcontroller(unit) as', () => {
       expect(controllerResult).instanceOf(Error);
     });
   });
+
+  describe('PUT /mails/{messageId}', () => {
+    it('throws error if attachment is not present', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const transaction = messageRepository.stubs.beginTransaction;
+      transaction.resolves(transactionStub);
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const controllerResult = await controller
+        .updateDraft(getSampleMailData, sampleMessageId)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+      sinon.assert.calledOnce(transaction);
+    });
+
+    it('throws error if mail is not present', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves();
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const controllerResult = await controller
+        .updateDraft(getSampleMailData, sampleMessageId)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+
+    it('throws error if mail is not draft', async () => {
+      setUpStub();
+      const messageChange = message;
+      messageChange.status = StatusMarker.send;
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(messageChange);
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const controllerResult = await controller
+        .updateDraft(getSampleMailData, sampleMessageId)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+  });
+
+  describe('POST /mails/{messageId}/attachments', () => {
+    it('throws error if attachment is not present', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const filter: Partial<Message> = {id: sampleMessageId};
+      const attachmentArray = [attachment];
+      const controllerResult = await controller
+        .addAttachment({attachments: attachmentArray}, sampleMessageId, filter)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+
+    it('throws error if message is not present', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves();
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const filter: Partial<Message> = {id: sampleMessageId};
+      const attachmentArray = [attachment];
+      const controllerResult = await controller
+        .addAttachment({attachments: attachmentArray}, sampleMessageId, filter)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+
+    it('throws error if message status is not draft', async () => {
+      setUpStub();
+      const messageChange = message;
+      messageChange.status = StatusMarker.send;
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(messageChange);
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const filter: Partial<Message> = {id: sampleMessageId};
+      const attachmentArray = [attachment];
+      const controllerResult = await controller
+        .addAttachment({attachments: attachmentArray}, sampleMessageId, filter)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+  });
+
+  describe('DELETE /mails/{messageId}/attachments/{attachmentId}', () => {
+    it('throws error if attachment is not present', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const filter: Partial<Message> = {id: sampleMessageId};
+      const controllerResult = await controller
+        .removeAttachment(sampleMessageId, sampleAttachmentId, filter)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+
+    it('throws error if message is not present', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves();
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const filter: Partial<Message> = {id: sampleMessageId};
+      const controllerResult = await controller
+        .removeAttachment(sampleMessageId, sampleAttachmentId, filter)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+
+    it('throws error if message status is not draft', async () => {
+      setUpStub();
+      const messageChange = message;
+      messageChange.status = StatusMarker.send;
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(messageChange);
+      const mailStub = messageRepository.stubs.updateById;
+      mailStub.resolves();
+      const filter: Partial<Message> = {id: sampleMessageId};
+      const controllerResult = await controller
+        .removeAttachment(sampleMessageId, sampleAttachmentId, filter)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+  });
+
   describe('DELETE /mails/bulk/{storage}/{action}', () => {
     it('trashes mails of inbox', async () => {
       setUpStub();
@@ -109,6 +250,46 @@ describe('originatorcontroller(unit) as', () => {
       expect(controllerResult).to.have.a.property('items').which.is.an.Array;
       sinon.assert.calledOnce(groupFind);
       sinon.assert.calledOnce(groupUpdate);
+    });
+    it('throws error if storage is in draft', async () => {
+      setUpStub();
+      const groupFind = groupRepository.stubs.find;
+      groupFind.resolves([group]);
+      const groupUpdate = groupRepository.stubs.update;
+      groupUpdate.resolves();
+      const storage: StorageMarker = StorageMarker.draft;
+      const action = 'trash';
+      const controllerResult = await controller
+        .trashBulk(
+          storage,
+          action,
+          {
+            extId: sampleExtId,
+          },
+          messageIds,
+        )
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+    it('throws error if storage is not trash or draft', async () => {
+      setUpStub();
+      const groupFind = groupRepository.stubs.find;
+      groupFind.resolves([group]);
+      const groupUpdate = groupRepository.stubs.update;
+      groupUpdate.resolves();
+      const storage: StorageMarker = StorageMarker.inbox;
+      const action = 'delete';
+      const controllerResult = await controller
+        .trashBulk(
+          storage,
+          action,
+          {
+            extId: sampleExtId,
+          },
+          messageIds,
+        )
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
     });
     it('throws an error if an group is not found', async () => {
       setUpStub();
@@ -172,6 +353,15 @@ describe('originatorcontroller(unit) as', () => {
       expect(controllerResult).instanceOf(Error);
       sinon.assert.calledOnce(groupFind);
     });
+    it('Throws error if group length is 0', async () => {
+      setUpStub();
+      const groupFind = groupRepository.stubs.find;
+      groupFind.resolves([]);
+      const controllerResult = await controller
+        .restore({}, messageIds)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
   });
   describe('PATCH mails/{messageId}/send', () => {
     it('Sends a Drafted message', async () => {
@@ -193,6 +383,24 @@ describe('originatorcontroller(unit) as', () => {
       sinon.assert.calledOnce(groupFindStub);
       sinon.assert.calledOnce(messageUpdateStub);
       sinon.assert.calledOnce(groupUpdateStub);
+    });
+    it('Sends a Drafted message with type changed', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const groupChange = group;
+      groupChange.type = PartyTypeMarker.bcc;
+      const groupFindStub = groupRepository.stubs.find;
+      groupFindStub.resolves([groupChange]);
+      const messageUpdateStub = messageRepository.stubs.updateById;
+      messageUpdateStub.resolves();
+      const groupUpdateStub = groupRepository.stubs.update;
+      groupUpdateStub.resolves();
+      const controllerResult = await controller.sendDraft(sampleMessageId, {
+        extId: sampleExtId,
+      });
+      // eslint-disable-next-line no-unused-expressions
+      expect(controllerResult).to.have.a.property('id').which.is.a.String;
     });
     it('Throws an Error if a message is not found', async () => {
       setUpStub();
@@ -220,6 +428,91 @@ describe('originatorcontroller(unit) as', () => {
       expect(controllerError).instanceOf(Error);
       sinon.assert.calledOnce(messageFindOneStub);
       sinon.assert.calledOnce(groupRepositoryStub);
+    });
+  });
+  describe('PATCH mails/marking/{markType}', () => {
+    it('Sends true for read category', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const groupFindStub = groupRepository.stubs.find;
+      groupFindStub.resolves([group]);
+      const messageUpdateStub = messageRepository.stubs.updateById;
+      messageUpdateStub.resolves();
+      const groupUpdateStub = groupRepository.stubs.update;
+      groupUpdateStub.resolves();
+      const controllerResult = await controller.markMail(
+        sampleMarkMail,
+        sampleIdArray,
+      );
+      // eslint-disable-next-line no-unused-expressions
+      expect(controllerResult).to.have.a.property('success').which.is.a.Boolean;
+    });
+    it('Sends true for unread category', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const groupFindStub = groupRepository.stubs.find;
+      groupFindStub.resolves([group]);
+      const messageUpdateStub = messageRepository.stubs.updateById;
+      messageUpdateStub.resolves();
+      const groupUpdateStub = groupRepository.stubs.update;
+      groupUpdateStub.resolves();
+      const controllerResult = await controller.markMail(
+        'unread',
+        sampleIdArray,
+      );
+      // eslint-disable-next-line no-unused-expressions
+      expect(controllerResult).to.have.a.property('success').which.is.a.Boolean;
+    });
+    it('Sends true for important category', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const groupFindStub = groupRepository.stubs.find;
+      groupFindStub.resolves([group]);
+      const messageUpdateStub = messageRepository.stubs.updateById;
+      messageUpdateStub.resolves();
+      const groupUpdateStub = groupRepository.stubs.update;
+      groupUpdateStub.resolves();
+      const controllerResult = await controller.markMail(
+        'important',
+        sampleIdArray,
+      );
+      // eslint-disable-next-line no-unused-expressions
+      expect(controllerResult).to.have.a.property('success').which.is.a.Boolean;
+    });
+    it('Sends true for not-important category', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const groupFindStub = groupRepository.stubs.find;
+      groupFindStub.resolves([group]);
+      const messageUpdateStub = messageRepository.stubs.updateById;
+      messageUpdateStub.resolves();
+      const groupUpdateStub = groupRepository.stubs.update;
+      groupUpdateStub.resolves();
+      const controllerResult = await controller.markMail(
+        'not-important',
+        sampleIdArray,
+      );
+      // eslint-disable-next-line no-unused-expressions
+      expect(controllerResult).to.have.a.property('success').which.is.a.Boolean;
+    });
+    it('Throws an Error if a category is not found', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const groupFindStub = groupRepository.stubs.find;
+      groupFindStub.resolves([group]);
+      const messageUpdateStub = messageRepository.stubs.updateById;
+      messageUpdateStub.resolves();
+      const groupUpdateStub = groupRepository.stubs.update;
+      groupUpdateStub.resolves();
+      const controllerResult = await controller
+        .markMail(VisibilityMarker.new, sampleIdArray)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
     });
   });
 });
--- services/in-mail-service/src/__tests__/unit/reply-and-forward.controller.unit.ts
+++ services/in-mail-service/src/__tests__/unit/reply-and-forward.controller.unit.ts
@@ -1,9 +1,11 @@
+import {Transaction} from '@loopback/repository';
 import {
   createStubInstance,
   expect,
   sinon,
   StubbedInstanceWithSinonAccessor,
 } from '@loopback/testlab';
+import {ForwardMailBody} from '../..';
 import {ReplyAndForwardController} from '../../controllers';
 import {
   AttachmentRepository,
@@ -11,9 +13,21 @@ import {
   MessageRepository,
   ThreadRepository,
 } from '../../repositories';
-import {getSampleMailData, user} from './sample-data';
+import {getSampleMailData, user, message, group, thread} from './sample-data';
 const sampleMessageId = 'sample-message-id';
 const sampleThreadId = 'sample-thread-id';
+async function commit() {}
+async function rollback() {}
+function isActive() {
+  return true;
+}
+const transaction: Transaction = {
+  id: 'test_id',
+  commit,
+  rollback,
+  isActive,
+};
+const forwardMailBody: ForwardMailBody = getSampleMailData;
 let messageRepository: StubbedInstanceWithSinonAccessor<MessageRepository>;
 let threadRepository: StubbedInstanceWithSinonAccessor<ThreadRepository>;
 let groupRepository: StubbedInstanceWithSinonAccessor<GroupRepository>;
@@ -43,6 +57,59 @@ describe('replyController(unit) as', () => {
         .catch(error => error);
       expect(controllerResult).instanceOf(Error);
       sinon.assert.calledOnce(messageFindOneStub);
+    });
+
+    it('returns an object that contains userIds', async () => {
+      setUpStub();
+      const messageFindOneStub = messageRepository.stubs.findOne;
+      messageFindOneStub.resolves(message);
+      const messageTransactionStub = messageRepository.stubs.beginTransaction;
+      messageTransactionStub.resolves(transaction);
+      const messageCreateRelationalStub =
+        messageRepository.stubs.createRelational;
+      messageCreateRelationalStub.resolves(message);
+      const groupFind = groupRepository.stubs.execute;
+      groupFind.resolves([group]);
+      const groupCreate = groupRepository.stubs.create;
+      groupCreate.resolves(group);
+      const controllerResult = await replyController
+        .replyMail(sampleThreadId, sampleMessageId, false, getSampleMailData)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Object);
+      sinon.assert.calledOnce(messageFindOneStub);
+    });
+  });
+
+  describe('PATCH threads/{threadId}/forward', () => {
+    it('Throws an error if a thread is not found', async () => {
+      setUpStub();
+      const controllerResult = await replyController
+        .forward(forwardMailBody, sampleThreadId)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
+    });
+
+    it('Throws error if there is no group id present', async () => {
+      setUpStub();
+      const threadCreateMulti = threadRepository.stubs.findOne;
+      threadCreateMulti.resolves(thread);
+      const threadCreate = threadRepository.stubs.incrementOrCreate;
+      threadCreate.resolves(thread);
+      const messageFindOneStub = messageRepository.stubs.create;
+      messageFindOneStub.resolves(message);
+      const messageTransactionStub = messageRepository.stubs.beginTransaction;
+      messageTransactionStub.resolves(transaction);
+      const messageCreateRelationalStub =
+        messageRepository.stubs.createRelational;
+      messageCreateRelationalStub.resolves(message);
+      const groupFind = groupRepository.stubs.execute;
+      groupFind.resolves([group]);
+      const groupCreate = groupRepository.stubs.create;
+      groupCreate.resolves(group);
+      const controllerResult = await replyController
+        .forward(forwardMailBody, sampleThreadId)
+        .catch(error => error);
+      expect(controllerResult).instanceOf(Error);
     });
   });
 });
