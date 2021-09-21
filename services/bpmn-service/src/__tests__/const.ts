320e513508ca0b73d0e3b6fcae2d77b9dd73024c
akshatdubeysf <77672713+akshatdubeysf@users.noreply.github.com>
2021-09-21 11:36:44 +0530

    feat(bpmn-service): add unit and acceptance tests (#289)
    GH-0

---
+++ services/bpmn-service/src/__tests__/const.ts
@@ -0,0 +1,18 @@
+import {WorkflowDto} from '../models';
+
+export const MOCK_CAMUNDA = 'https://mock-camunda.api/engine-rest';
+
+export const firstTestBpmn: WorkflowDto = new WorkflowDto({
+  name: 'first-bpmn',
+  bpmnFile: JSON.stringify(['topic1', 'topic2']),
+  inputSchema: {
+    type: 'object',
+    properties: {valueA: {type: 'string'}, valueB: {type: 'string'}},
+    required: ['valueA', 'valueB'],
+  },
+});
+
+export const firstTestBpmnInput = {
+  valueA: 'string',
+  valueB: 'string',
+};
