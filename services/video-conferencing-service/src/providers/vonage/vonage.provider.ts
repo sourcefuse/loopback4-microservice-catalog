07c07197878d33a83d989e49c6fae91e9b771716
parul147 <86265316+parul147@users.noreply.github.com>
2021-09-24 19:22:29 +0530

    Update vonage.provider.ts

--- services/video-conferencing-service/src/providers/vonage/vonage.provider.ts
+++ services/video-conferencing-service/src/providers/vonage/vonage.provider.ts
@@ -1,9 +1,9 @@
-import { Provider, service } from "@loopback/core";
+import {Provider, service} from '@loopback/core';
 import {
   ArchiveResponse,
   ArchiveResponseList,
   SessionResponse,
-} from "../../types";
+} from '../../types';
 import {
   VonageVideoChat,
   VonageMeetingOptions,
@@ -11,36 +11,38 @@ import {
   VonageSessionOptions,
   VonageS3TargetOptions,
   VonageAzureTargetOptions,
-} from "./types";
+} from './types';
 
-import { VonageService } from "./vonage.service";
-
-
+import {VonageService} from './vonage.service';
 
 export class VonageProvider implements Provider<VonageVideoChat> {
   constructor(
     @service(VonageService)
-    private readonly vonageService: VonageService
+    private readonly vonageService: VonageService,
   ) {}
   value() {
     return {
       getMeetingLink: async (
-        meetingOptions: VonageMeetingOptions): Promise<VonageMeetingResponse> => this.vonageService.getMeetingLink(meetingOptions),
-      
+        meetingOptions: VonageMeetingOptions,
+      ): Promise<VonageMeetingResponse> =>
+        this.vonageService.getMeetingLink(meetingOptions),
+
       getToken: async (
         sessionId: string,
-        options: VonageSessionOptions
-      ): Promise<SessionResponse> => this.vonageService.getToken(sessionId, options),
-      
+        options: VonageSessionOptions,
+      ): Promise<SessionResponse> =>
+        this.vonageService.getToken(sessionId, options),
+
       getArchives: async (
-        archiveId: string | null
-      ): Promise<ArchiveResponse | ArchiveResponseList> => this.vonageService.getArchives(archiveId),
-      
+        archiveId: string | null,
+      ): Promise<ArchiveResponse | ArchiveResponseList> =>
+        this.vonageService.getArchives(archiveId),
+
       deleteArchive: async (archiveId: string) => {
         await this.vonageService.deleteArchive(archiveId);
       },
       setUploadTarget: async (
-        storageConfig: VonageS3TargetOptions | VonageAzureTargetOptions
+        storageConfig: VonageS3TargetOptions | VonageAzureTargetOptions,
       ): Promise<void> => {
         await this.vonageService.setUploadTarget(storageConfig);
       },
