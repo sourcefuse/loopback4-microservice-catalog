5ce275f90e58258e1c5ba284a37a0c2d53c6f6f7
Jyoti-13 <jyoti.bansal@sourcefuse.com>
2021-09-27 01:01:39 +0530

    fix(in-mail-service): issue #338 bug resolved

--- sandbox/in-mail-example/src/datasources/inmail.pg.datasource.ts
+++ sandbox/in-mail-example/src/datasources/inmail.pg.datasource.ts
@@ -7,11 +7,12 @@ import InMailDatasourceName from '@sourceloop/in-mail-service';
 const config = {
   name: InMailDatasourceName,
   connector: 'postgresql',
-  host: "localhost",
-  port: 5432,
-  user: "jyotidb",
-  password: "admin",
-  database: "inmail",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  schema: process.env.DB_SCHEMA,
  
 };
 
