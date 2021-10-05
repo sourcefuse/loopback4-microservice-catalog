1b0be153256fd645cc944b052f46b3757e557a75
unknown <sumiter92@gmail.com>
2021-10-05 16:08:49 +0530

    refactor(payment-service): fix build fail

--- services/payment-service/src/repositories/subscriptions.repository.ts
+++ services/payment-service/src/repositories/subscriptions.repository.ts
@@ -1,12 +1,11 @@
 import {inject} from '@loopback/core';
 import {DefaultCrudRepository, juggler} from '@loopback/repository';
 import {PaymentDatasourceName} from '../keys';
-import {Subscriptions, SubscriptionsRelations} from '../models';
+import {Subscriptions} from '../models';
 
 export class SubscriptionsRepository extends DefaultCrudRepository<
   Subscriptions,
-  typeof Subscriptions.prototype.id,
-  SubscriptionsRelations
+  typeof Subscriptions.prototype.id
 > {
   constructor(
     @inject(`datasources.${PaymentDatasourceName}`)
