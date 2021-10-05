392afc276a6a95e4779fbd659f3e551514ef97a4
Jyoti-13 <jyoti.bansal@sourcefuse.com>
2021-10-06 00:48:39 +0530

    feat(hidden-api): adds hidden-apifeature (#143)

--- services/enhancer-keys.ts
+++ services/enhancer-keys.ts
@@ -3,12 +3,10 @@ import {BindingKey} from '@loopback/context';
 const hiddenKey = 'sf.oas.hiddenEndpoints';
 
 export type OasHiddenApi = {
-  path: string,
+  path: string;
   httpMethod: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
-}
+};
 
 export namespace OASBindings {
-  export const HiddenEndpoint = BindingKey.create<
-    OasHiddenApi[]
-  >(hiddenKey);
-};
+  export const HiddenEndpoint = BindingKey.create<OasHiddenApi[]>(hiddenKey);
+}
