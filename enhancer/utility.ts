e376992d9e0ce29ad738f6dd1ac1d99d035a197c
Jyoti-13 <jyoti.bansal@sourcefuse.com>
2021-10-06 01:33:49 +0530

    feat(hidden-api): adds hidden-api feature (#143)

---
+++ enhancer/utility.ts
@@ -0,0 +1,35 @@
+import {PathsObject} from 'openapi3-ts/dist/model/OpenApi';
+import {OasHiddenApi} from '../services/enhancer-key';
+
+function apiSearchFunction(
+  apiSearch: [string, OasHiddenApi],
+  path: PathsObject,
+) {
+  if (path['get'] && apiSearch[1]['httpMethod'] === 'GET') {
+    delete path['get'];
+  }
+  if (path['post'] && apiSearch[1]['httpMethod'] === 'POST') {
+    delete path['post'];
+  }
+  if (path['patch'] && apiSearch[1]['httpMethod'] === 'PATCH') {
+    delete path['patch'];
+  }
+  if (path['delete'] && apiSearch[1]['httpMethod'] === 'DELETE') {
+    delete path['delete'];
+  }
+  if (path['put'] && apiSearch[1]['httpMethod'] === 'PUT') {
+    delete path['put'];
+  }
+}
+export function apiHide(
+  arrayApiSearch: [string, OasHiddenApi][],
+  paths: PathsObject,
+) {
+  arrayApiSearch.forEach(apiSearch => {
+    for (const path in paths) {
+      if (path === apiSearch[1]['path']) {
+        apiSearchFunction(apiSearch, paths[path]);
+      }
+    }
+  });
+}
