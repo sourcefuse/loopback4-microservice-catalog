4dd86eb4e5711754ae0462bd81632414294ce84b
Samarpan <samarpan.bhattacharya@sourcefuse.com>
2021-10-27 17:24:47 +0530

    fix(search-client): fix sonar
    gh-

--- packages/search/projects/search-lib/src/lib/search-lib.module.ts
+++ packages/search/projects/search-lib/src/lib/search-lib.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {SearchComponent} from './search/search.component';
 import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {MatSelectModule} from '@angular/material/select';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatSelectModule} from '@angular/material/select';
+
+import {SearchComponent} from './search/search.component';
 
 @NgModule({
   declarations: [SearchComponent],
