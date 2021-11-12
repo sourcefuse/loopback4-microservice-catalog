03b235cea74f9a2a4628a6ca028ded4237278902
jamescrowley321 <jamescrowley151@gmail.com>
2021-11-12 07:58:11 -0600

    chore(sandbox): cleanup-tf
    cleanup-tf

--- sandbox/k8s/tf-sourceloop-sandbox/ingress.tf
+++ sandbox/k8s/tf-sourceloop-sandbox/ingress.tf
@@ -1,12 +1,6 @@
-data "template_file" "ingress_init" {
-  template = file("${path.module}/ingress/sourceloop-sandbox-ingress.yaml")
-  vars = {
-    namespace = var.namespace_name
-  }
-}
-
-
 resource "kubectl_manifest" "sandbox_ingress" {
-  yaml_body  = data.template_file.ingress_init.rendered
+  yaml_body  = templatefile("${path.module}/ingress/sourceloop-sandbox-ingress.yaml", {
+    namespace = var.namespace_name
+  })
   depends_on = [kubernetes_namespace.sourceloop_sandbox]
 }
