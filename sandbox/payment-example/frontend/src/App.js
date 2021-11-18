1125e10f11951b8bae14b38ac6adf45b0526a2b5
unknown <sumiter92@gmail.com>
2021-11-18 15:25:59 +0530

    refactor(sandbox): comments resolved

--- sandbox/payment-example/frontend/src/App.js
+++ sandbox/payment-example/frontend/src/App.js
@@ -1,29 +1,36 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import SubscriptionPage  from './SubscriptionPage';
+import SubscriptionPage from './SubscriptionPage';
 import CheckoutPage from './CheckoutPage';
 import OrdersPage from './OrdersPage';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import dotenv from 'dotenv';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+dotenv.config();
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/"
-             render={(props) => <SubscriptionPage {...props} />}/>
-            <Route path="/checkout"
-            render={(props) => <CheckoutPage {...props} />}/>
-            <Route exact path="/orders"
-            render={(props) => <OrdersPage {...props} />}/>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+    <Router>
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={props => <SubscriptionPage {...props} />}
+          />
+          <Route
+            path="/checkout"
+            render={props => <CheckoutPage {...props} />}
+          />
+          <Route
+            exact
+            path="/orders"
+            render={props => <OrdersPage {...props} />}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 export default App;
--- sandbox/payment-example/frontend/src/OrdersPage.js
+++ sandbox/payment-example/frontend/src/OrdersPage.js
@@ -10,106 +10,97 @@ import {
   MDBCardFooter,
   MDBBtn,
 } from 'mdbreact';
-import {useHistory} from "react-router-dom";
-
-
+import {useHistory} from 'react-router-dom';
 
 const OrdersPage = () => {
-const history = useHistory();
-function handleCheckOut(amount){
-
-  const location = {
+  const history = useHistory();
+  function handleCheckOut(amount) {
+    const location = {
       pathname: '/checkout',
-      state: { amount: amount , type : 'orders'}
+      state: {amount: amount, type: 'orders'},
     };
     history.push(location);
+  }
 
-}
+  const productList = [
+    {
+      name: 'Shoes',
+      category: 'Leather boots',
+      imageUrl:
+        'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img%20(39).jpg',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi,veritatis',
+      price: 6900,
+    },
+    {
+      name: 'Jeans',
+      category: 'Slim Jeans',
+      imageUrl:
+        'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img%20(22).jpg',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi,veritatis',
+      price: 9900,
+    },
+    {
+      name: 'Shorts',
+      category: 'Denim Shorts',
+      imageUrl: 'https://mdbootstrap.com/img/Photos/Others/img%20(31).jpg',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi,veritatis',
+      price: 4900,
+    },
+  ];
+
   return (
-    <section className='text-center my-5'>
-      <h2 className='h1-responsive font-weight-bold text-center my-5'>Our Bestsellers</h2>
-      <p className='grey-text text-center w-responsive mx-auto mb-5'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis
-        totam voluptas nostrum quisquam eum porro a pariatur veniam.
+    <section className="text-center my-5">
+      <h2 className="h1-responsive font-weight-bold text-center my-5">
+        Our Bestsellers
+      </h2>
+      <p className="grey-text text-center w-responsive mx-auto mb-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error
+        amet numquam iure provident voluptate esse quasi, veritatis totam
+        voluptas nostrum quisquam eum porro a pariatur veniam.
       </p>
-          <MDBRow style={{marginLeft:'30px', marginRight:'30px'}}>
-              <MDBCol md='4'>
-                <MDBCard narrow ecommerce className='mb-2'>
-                  <MDBCardImage
-                    cascade
-                    top
-                    src='https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img%20(39).jpg'
-                    alt='sample photo'
-                  />
-                  <MDBCardBody cascade>
-                    <a href='#!' className='text-muted'>
-                      <h5>Shoes</h5>
-                    </a>
-                    <MDBCardTitle>
-                      <strong>
-                        <a href='#!'>Leather boots</a>
-                      </strong>
-                    </MDBCardTitle>
-                    <MDBCardText>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci.</MDBCardText>
-                    <MDBCardFooter className='px-1'>
-                      <span className='float-left'>6900 ₹</span>
-                      <span className='float-right'>
-                         <MDBBtn gradient="peach" onClick={() => { handleCheckOut(6900) }} >Buy Now</MDBBtn>
-                      </span>
-                    </MDBCardFooter>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol md='4' className='clearfix d-none d-md-block'>
-                <MDBCard narrow ecommerce className='mb-2'>
-                  <MDBCardImage
-                    cascade
-                    top
-                    src='https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img%20(22).jpg'
-                    alt='sample photo'
-                  />
-                  <MDBCardBody cascade>
-                    <a href='#!' className='text-muted'>
-                      <h5>Jeans</h5>
-                    </a>
-                    <MDBCardTitle>
-                      <strong>
-                        <a href='#!'>Slim jeans</a>
-                      </strong>
-                    </MDBCardTitle>
-                    <MDBCardText>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci.</MDBCardText>
-                    <MDBCardFooter className='px-1'>
-                      <span className='float-left'>9900 ₹</span>
-                      <span className='float-right'>
-                         <MDBBtn gradient="peach" onClick={() => { handleCheckOut(9900) }}>Buy Now</MDBBtn>
-                      </span>
-                    </MDBCardFooter>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-              <MDBCol md='4' className='clearfix d-none d-md-block'>
-                <MDBCard narrow ecommerce className='mb-2'>
-                  <MDBCardImage cascade top src='https://mdbootstrap.com/img/Photos/Others/img%20(31).jpg' alt='sample photo' />
-                  <MDBCardBody cascade>
-                    <a href='#!' className='text-muted'>
-                      <h5>Shorts</h5>
-                    </a>
-                    <MDBCardTitle>
-                      <strong>
-                        <a href='#!'>Denim shorts</a>
-                      </strong>
-                    </MDBCardTitle>
-                    <MDBCardText>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci.</MDBCardText>
-                    <MDBCardFooter className='px-1'>
-                      <span className='float-left'>4900 ₹</span>
-                      <span className='float-right'>
-                    <MDBBtn gradient="peach" onClick={() => { handleCheckOut(4900) }}>Buy Now</MDBBtn>
-                      </span>
-                    </MDBCardFooter>
-                  </MDBCardBody>
-                </MDBCard>
-              </MDBCol>
-          </MDBRow>
+      <MDBRow style={{marginLeft: '30px', marginRight: '30px'}}>
+        {productList.map((product, index) => {
+          return (
+            <MDBCol md="4">
+              <MDBCard narrow ecommerce className="mb-2">
+                <MDBCardImage
+                  cascade
+                  top
+                  src={product.imageUrl}
+                  alt="sample photo"
+                />
+                <MDBCardBody cascade>
+                  <a href="#!" className="text-muted">
+                    <h5>{product.name}</h5>
+                  </a>
+                  <MDBCardTitle>
+                    <strong>
+                      <a href="#!">{product.category}</a>
+                    </strong>
+                  </MDBCardTitle>
+                  <MDBCardText>{product.description}</MDBCardText>
+                  <MDBCardFooter className="px-1">
+                    <span className="float-left">{product.price} ₹</span>
+                    <span className="float-right">
+                      <MDBBtn
+                        gradient="peach"
+                        onClick={() => {
+                          handleCheckOut(productList[index].price);
+                        }}
+                      >
+                        Buy Now
+                      </MDBBtn>
+                    </span>
+                  </MDBCardFooter>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          );
+        })}
+      </MDBRow>
     </section>
   );
 };
