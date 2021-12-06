09ba2b4a680f58f99ab566c4a0516e526eb02ada
Ankur <ankur.bansal@sourcefuse.com>
2021-12-06 15:06:52 +0530

    feat(sandbox): added UI to chat example
    gh-345

--- sandbox/chat-notification-pubnub-example/frontend/karma.conf.js
+++ sandbox/chat-notification-pubnub-example/frontend/karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['@angular-devkit/build-angular','jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -13,12 +13,6 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
@@ -32,7 +26,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['kjhtml','progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
