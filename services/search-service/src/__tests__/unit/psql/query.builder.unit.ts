2de7d259845d66eadf68c5bd02269c2b2898f1b1
Akshat  Dubey <akshat.dubey@sourcefuse.com>
2022-01-11 12:19:34 +0530

    fix(search-service): fix export of classes, fix search source name in query
    GH-0

--- services/search-service/src/__tests__/unit/psql/query.builder.unit.ts
+++ services/search-service/src/__tests__/unit/psql/query.builder.unit.ts
@@ -1,32 +1,56 @@
-import {BuilderTest} from '../types';
 import {buildTestsRunner} from '../runner';
 import {PsqlQueryBuilder} from '../../../classes';
 import {expect} from '@loopback/testlab';
-import {testModelList} from '../..';
+import {testModelList, testModelListWithIdentifier} from '../..';
 
 describe('PostgreSQL QueryBuilder', () => {
-  const testList: Array<BuilderTest> = [
-    {
-      params: {},
-      it: 'should build a query with no options',
-      expects:
-        "(SELECT description, name, 'TestSearched' as source, ts_rank_cd(to_tsvector(public.f_concat_ws(' ', description, name)), to_tsquery($1)) as rank" +
-        " from public.TestSearched where to_tsvector(public.f_concat_ws(' ', description, name)) @@ to_tsquery($1))" +
-        ' UNION ALL ' +
-        "(SELECT about as description, identifier as name, 'TestSearchedCustom' as source, ts_rank_cd(to_tsvector(public.f_concat_ws(' ', about, identifier)), to_tsquery($1)) as rank" +
-        " from public.TestSearchedCustom where to_tsvector(public.f_concat_ws(' ', about, identifier)) @@ to_tsquery($1))" +
-        ' ORDER BY rank DESC',
-    },
-  ];
   describe(
     'with match parameter',
     buildTestsRunner(
       PsqlQueryBuilder,
-      testList,
+      [
+        {
+          params: {},
+          it: 'should build a query with no options',
+          expects:
+            "(SELECT description, name, 'TestSearched' as source, ts_rank_cd(to_tsvector(public.f_concat_ws(' ', description, name)), to_tsquery($1)) as rank" +
+            " from public.TestSearched where to_tsvector(public.f_concat_ws(' ', description, name)) @@ to_tsquery($1))" +
+            ' UNION ALL ' +
+            "(SELECT about as description, identifier as name, 'TestSearchedCustom' as source, ts_rank_cd(to_tsvector(public.f_concat_ws(' ', about, identifier)), to_tsquery($1)) as rank" +
+            " from public.TestSearchedCustom where to_tsvector(public.f_concat_ws(' ', about, identifier)) @@ to_tsquery($1))" +
+            ' ORDER BY rank DESC',
+        },
+      ],
       'match',
       ['match:*'],
       expect,
       testModelList,
+    ),
+  );
+  describe(
+    'with custom identifier',
+    buildTestsRunner(
+      PsqlQueryBuilder,
+      [
+        {
+          params: {},
+          it: 'should build a query with no options',
+          expects:
+            "(SELECT description, name, 'TestSearched' as source, ts_rank_cd(to_tsvector(public.f_concat_ws(' ', description, name)), to_tsquery($1)) as rank" +
+            " from public.TestSearched where to_tsvector(public.f_concat_ws(' ', description, name)) @@ to_tsquery($1))" +
+            ' UNION ALL ' +
+            "(SELECT about as description, identifier as name, 'TestSearchedCustom' as source, ts_rank_cd(to_tsvector(public.f_concat_ws(' ', about, identifier)), to_tsquery($1)) as rank" +
+            " from public.TestSearchedCustom where to_tsvector(public.f_concat_ws(' ', about, identifier)) @@ to_tsquery($1))" +
+            ' UNION ALL ' +
+            "(SELECT about as description, identifier as name, 'CustomIdentifier' as source, ts_rank_cd(to_tsvector(public.f_concat_ws(' ', about, identifier)), to_tsquery($1)) as rank" +
+            " from public.TestSearchedCustom where to_tsvector(public.f_concat_ws(' ', about, identifier)) @@ to_tsquery($1))" +
+            ' ORDER BY rank DESC',
+        },
+      ],
+      'match',
+      ['match:*'],
+      expect,
+      testModelListWithIdentifier,
     ),
   );
 });
