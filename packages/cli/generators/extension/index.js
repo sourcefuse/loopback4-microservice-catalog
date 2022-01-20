4c344fc5421b72d2dcca8dd1ad2ba9681798303c
Jyoti-13 <jyoti.bansal@sourcefuse.com>
2022-01-20 19:52:01 +0530

    feat(microservice): sourceloop-cli extension update

--- packages/cli/generators/extension/index.js
+++ packages/cli/generators/extension/index.js
@@ -6,6 +6,7 @@
 'use strict';
 const ExtensionGenerator = require('@loopback/cli/generators/extension');
 const fs = require('fs');
+const {spawn} = require('child_process');
 const path = require('path');
 
 module.exports = class EGenerator extends ExtensionGenerator {
@@ -62,12 +63,50 @@ module.exports = class EGenerator extends ExtensionGenerator {
     const packageJsonFile = path.join(process.cwd(), 'package.json');
     const packageJson = require(packageJsonFile);
     const scripts = packageJson.scripts;
+    this._setupExtension(packageJson.name);
     scripts.preinstall =
       'npm i @loopback/build@6.4.0 --no-save && npm run build';
     scripts.prune = 'npm prune --production';
     packageJson.scripts = scripts;
     fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson), null, 2);
     return super.install();
+  }
+
+  _setupExtension(packageName){
+    this._loopbackCore(packageName).then(() =>
+      this._tslib(packageName),
+    )
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  async _loopbackCore(packageName){
+    await this._spawnProcess('npm', ['i', '@loopback/core'], {packageName});
+  }
+
+  async _tslib(packageName){
+    await this._spawnProcess('npm', ['i', 'tslib'], {packageName});
+  }
+
+  _spawnProcess(cmd, cmdArgs, cwd) {
+    return new Promise((resolve, reject) => {
+      const spawnedProcess = spawn(cmd, cmdArgs, {...cwd});
+
+      spawnedProcess.stdout.on("data", data => {
+          console.log(`stdout: ${data}`);
+      });
+      spawnedProcess.stderr.on("data", data => {
+          console.log(`stderr: ${data}`);
+      });
+      spawnedProcess.on('error', (error) => {
+          console.log(`error: ${error.message}`);
+          reject(error);
+      });
+      spawnedProcess.on("close", code => {
+          resolve();
+      });
+    })
   }
 
   end() {
