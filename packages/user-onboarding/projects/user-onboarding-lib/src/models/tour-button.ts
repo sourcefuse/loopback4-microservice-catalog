8a819ea5324b9321b68cb6293c0690dc1249f2f5
barleendhaliwal <85155575+barleendhaliwal@users.noreply.github.com>
2022-01-25 15:05:26 +0530

    fix(user-onboarding): add max wait time, add observable for error handling in case tour fails to start (#491)
    gh-0
    
    Co-authored-by: akshatdubeysf <77672713+akshatdubeysf@users.noreply.github.com>

--- packages/user-onboarding/projects/user-onboarding-lib/src/models/tour-button.ts
+++ packages/user-onboarding/projects/user-onboarding-lib/src/models/tour-button.ts
@@ -1,5 +1,7 @@
+import Step from 'shepherd.js/src/types/step';
+
 export interface TourButton {
-  action: () => void;
+  action?: () => Step.StepOptionsButton['action'];
   classes?: string;
   text: string;
   key?: string;
