0ae665204c72a32399a000c04d8ea368bdccbeb4
Jyoti-13 <jyoti.bansal@sourcefuse.com>
2022-03-09 15:37:13 +0530

    feat(cli): add facade feature to sourceloop cli

--- packages/cli/generators/microservice/index.js
+++ packages/cli/generators/microservice/index.js
@@ -20,6 +20,26 @@ module.exports = class MGenerator extends AppGenerator {
     super(args, opts);
   }
 
+  dirChoice = ['facade',
+             'service']
+
+  async setWorkingDir(){
+    this.dirAnswer = await this.prompt([
+      {
+        type: 'list',
+        name: 'dirName',
+        message: 'Select the project type:',
+        choices: this.dirChoice,
+      }
+    ]);
+    if(this.dirAnswer.dirName === 'facade'){
+      process.chdir('./facades');
+    }
+    else{
+      process.chdir('./services')
+    }
+  }
+
   _setupGenerator() {
     super.option('serviceDependency', {
       type : String,
@@ -62,29 +82,76 @@ module.exports = class MGenerator extends AppGenerator {
                     'video-conferencing-service'];
 
   async promptUniquePrefix() {
+    this.projectInfo.dirName = this.dirAnswer.dirName;
     this.answers = await this.prompt([
-        {
-            type: 'input',
-            name: 'uniquePrefix',
-            message: 'Unique prefix for the docker image:'
-        },
-        {
-            type: 'input',
-            name: 'serviceSelect',
-            message: 'Do you want to add sourceloop dependencies?(y/n)'
-        }
+      {
+        type: 'input',
+        name: 'uniquePrefix',
+        message: 'Unique prefix for the docker image:',
+      },
+      {
+        type: 'input',
+        name: 'serviceSelect',
+        message: 'Do you want to add sourceloop dependencies?(y/n)',
+      },
     ]);
 
-    if(this.answers.serviceSelect === 'y' || this.answers.serviceSelect === 'Y') {
+    if(this.dirAnswer.dirName === 'service'){
+      this.serviceDbAnswers = await this.prompt([
+        {
+          type: 'input',
+          name: 'dbName',
+          message: 'Datasource name:',
+        },
+        {
+          type: 'list',
+          name: 'dbConnector',
+          message: 'Select the connector:',
+          choices: this.connectorChoices,
+        },
+        {
+          type: 'input',
+          name: 'migratedb',
+          message: 'Do you want to add migration?(y/n)',
+        }
+      ]);
+      function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+  
+      this.projectInfo.datasourceName = this.serviceDbAnswers.dbName;
+      this.projectInfo.datasourceClassName = capitalizeFirstLetter(
+        this.serviceDbAnswers.dbName,
+      );
+      this.projectInfo.datasourceConnectorName = this.serviceDbAnswers.dbConnector;
+    }
+
+
+    if (
+      this.answers.serviceSelect === 'y' ||
+      this.answers.serviceSelect === 'Y'
+    ) {
       this.service = await this.prompt([
         {
           name: 'selector',
           message: 'Select the service you want to add:',
           type: 'list',
           choices: this.serviceChoices,
-        },
+        }
       ]);
-    this.projectInfo.serviceDependency = this.service.selector;
+      this.projectInfo.serviceDependency = this.service.selector;
+      if(this.dirAnswer.dirName === 'service'){
+        if(this.serviceDbAnswers.migratedb === 'y' || this.serviceDbAnswers.migratedb === 'Y'){
+          this.copyMigratePrompt = await this.prompt([
+            {
+              type: 'list',
+              name: 'copymigratedb',
+              message: 'Which type of migration do you want:',
+              choices:  this.migrationChoices,
+            }
+          ]);
+        }
+      }
     }
   }
 
@@ -190,9 +257,87 @@ module.exports = class MGenerator extends AppGenerator {
     logBar(bar1);
   }
 
+  async _changeFileName(){
+    if(this.dirAnswer.dirName === 'service'){
+      const oldPath = path.join(process.cwd(),"./src/datasources/datasource.ts");
+      const newPath = path.join(process.cwd(),`./src/datasources/${this.serviceDbAnswers.dbName}.datasource.ts`);
+      fs.renameSync(oldPath,newPath);
+    }
+    else{
+      const datasourcesPath = path.join(process.cwd(),'./src/datasources');
+      fs.rmSync(datasourcesPath, {recursive: true});
+      const repositoryPath = path.join(process.cwd(),'./src/repositories');
+      fs.rmSync(repositoryPath, {recursive: true});
+    }
+    logBar(bar1);
+  }
+
   async _addDependency(packageName){
     if(this.answers.serviceSelect === 'y' || this.answers.serviceSelect === 'Y'){
       await spawnProcess('npx', ['lerna', 'add', '@sourceloop/'+`${this.service.selector}`, '--scope='+`${packageName}`], {packageName});
+    }
+    logBar(bar1);
+  }
+
+  async _addMigrations(){
+    if(this.dirAnswer.dirName === 'service'){
+      if(this.serviceDbAnswers.migratedb === 'y' || this.serviceDbAnswers.migratedb === 'Y'){
+        const migrationScope = '--scope=migrations';
+         await spawnProcess('npx', ['lerna', 'create', 'migrations', '-y'], {cwd: process.cwd()});
+         await spawnProcess('npx', ['lerna', 'add', 'db-migrate', migrationScope], {cwd: process.cwd()});
+         await spawnProcess('npx', ['lerna', 'add', 'dotenv', migrationScope], {cwd: process.cwd()});
+         await spawnProcess('npx', ['lerna', 'add', 'dotenv-extended', migrationScope], {cwd: process.cwd()});
+         await spawnProcess('npx', ['lerna', 'add', '-D', '@types/dotenv', migrationScope], {cwd: process.cwd()});
+         await spawnProcess('npx', ['lerna', 'add', '-D', 'npm-run-all', migrationScope], {cwd: process.cwd()});
+   
+         if(this.serviceDbAnswers.dbConnector === 'postgresql'){
+           await spawnProcess('npx', ['lerna', 'add', 'db-migrate-pg', migrationScope], {cwd: process.cwd()});
+         }
+         else{
+           await spawnProcess('npx', ['lerna', 'add', 'db-migrate-mysql', migrationScope], {cwd: process.cwd()});
+         }
+         fs.rmSync(path.join(process.cwd(), '../../packages/migrations/__tests__'), {recursive: true});
+         fs.rmSync(path.join(process.cwd(), '../../packages/migrations/lib'), {recursive: true});
+         const packageJsonFile = path.join(process.cwd(),'../../packages/migrations/package.json');
+         const packageJson = require(packageJsonFile);
+         const scripts = packageJson.scripts;
+         scripts["db:migrate"] = "run-s db:migrate:*";
+         scripts['db:migrate-down']= "run-s db:migrate-down:*";
+         scripts['db:migrate-reset'] = "run-s db:migrate-reset:*";
+         packageJson.scripts = scripts;
+         fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson), null, 2);
+       }
+    }
+    logBar(bar1);
+  }
+
+  async _copyMigrations(){
+    if(this.dirAnswer.dirName === 'service'){
+      const folderPath = path.join(process.cwd(),'../../packages/migrations',this.projectInfo.applicationName.substr(0,this.projectInfo.applicationName.indexOf('A')));
+      fs.mkdirSync(folderPath);
+
+      if(this.copyMigratePrompt.copymigratedb === 'custom'){
+        const sourceFolder = this.templatePath('../databasetemplate/database.json');
+        const destinationFolder = path.join(folderPath,'database.json');
+        fse.copySync(sourceFolder,destinationFolder);
+      }
+
+      else{
+        const sourcePath = this.templatePath('../databasetemplate/databasesourceloop.json');
+        const destinationPath = path.join(folderPath,'database.json');
+        fse.copySync(sourcePath,destinationPath);
+        const database = fs.readFileSync(destinationPath,{encoding:'utf8', flag:'r'});
+        if(this.serviceDbAnswers.dbConnector === 'postgresql'){
+          const replaceConnector = database.replace(/<DbKey>/g,'pg');
+          fs.writeFileSync(destinationPath, replaceConnector, {encoding:'utf8'});
+        }
+        else{
+          const replaceConnector = database.replace(/<DbKey>/g,'mysql');
+          fs.writeFileSync(destinationPath, replaceConnector, {encoding:'utf8'});
+        }
+        const serviceMigration = path.join(process.cwd(),`./node_modules/@sourceloop/${this.service.selector}/migrations`);
+        fse.copySync(serviceMigration,folderPath);
+      }
     }
     logBar(bar1);
   }
