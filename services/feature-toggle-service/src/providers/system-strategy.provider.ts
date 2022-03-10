5e6e52184ce7bba796099c9beac5e848676bd778
Ankur <ankur.bansal@sourcefuse.com>
2022-03-10 19:25:42 +0530

    feat(feature-toggle-service): removed unleash

--- services/feature-toggle-service/src/providers/system-strategy.provider.ts
+++ services/feature-toggle-service/src/providers/system-strategy.provider.ts
@@ -1,66 +1,17 @@
-import {Getter, inject, Provider} from '@loopback/core';
-import {repository} from '@loopback/repository';
-import {StrategyBindings} from '../keys';
-import {
-  FeatureRepository,
-  FeatureToggleRepository,
-  StrategyRepository,
-} from '../repositories';
-import {FeatureFlagMetadata, FeatureInterface} from '../types';
+import {inject, Provider} from '@loopback/core';
+import {StrategyHelperService} from '../services';
+import {FeatureInterface} from '../types';
 
 export class SystemStrategyProvider implements Provider<FeatureInterface> {
   constructor(
-    @inject.getter(StrategyBindings.METADATA)
-    private readonly getMetadata: Getter<FeatureFlagMetadata>,
-    @repository(FeatureRepository)
-    public featureRepository: FeatureRepository,
-    @repository(FeatureToggleRepository)
-    public featureToggleRepository: FeatureToggleRepository,
-    @repository(StrategyRepository)
-    public strategyRepository: StrategyRepository,
+    @inject('services.StrategyHelperService')
+    private readonly strategyHelperService: StrategyHelperService,
   ) {}
   value(): FeatureInterface {
     return () => this.isEnabled();
   }
 
   async isEnabled() {
-    const metadata: FeatureFlagMetadata = await this.getMetadata();
-    const featureName = metadata.feature;
-
-    const feature = await this.featureRepository.findOne({
-      where: {
-        name: featureName,
-      },
-    });
-    if (!feature) {
-      return false;
-    }
-
-    const featureToggle = await this.featureToggleRepository.findOne({
-      where: {
-        featureId: feature.id,
-        itemId: feature.id,
-      },
-    });
-
-    //When toggle is not set
-    if (!featureToggle) {
-      const strategy = await this.strategyRepository.findOne({
-        where: {
-          name: 'System',
-        },
-      });
-
-      await this.featureToggleRepository.create({
-        featureId: feature?.id,
-        strategyId: strategy?.id,
-        itemId: feature?.id,
-        status: feature.defaultValue,
-      });
-
-      return feature.defaultValue;
-    }
-
-    return featureToggle.status;
+    return this.strategyHelperService.isFeatureEnabled('System');
   }
 }
--- services/feature-toggle-service/src/providers/tenant-strategy.provider.ts
+++ services/feature-toggle-service/src/providers/tenant-strategy.provider.ts
@@ -1,70 +1,17 @@
-import {Getter, inject, Provider} from '@loopback/core';
-import {repository} from '@loopback/repository';
-import {IAuthUserWithPermissions} from '@sourceloop/core';
-import {AuthenticationBindings} from 'loopback4-authentication';
-import {StrategyBindings} from '../keys';
-import {
-  FeatureRepository,
-  FeatureToggleRepository,
-  StrategyRepository,
-} from '../repositories';
-import {FeatureFlagMetadata, FeatureInterface} from '../types';
+import {inject, Provider} from '@loopback/core';
+import {StrategyHelperService} from '../services';
+import {FeatureInterface} from '../types';
 
 export class TenantStrategyProvider implements Provider<FeatureInterface> {
   constructor(
-    @inject.getter(StrategyBindings.METADATA)
-    private readonly getMetadata: Getter<FeatureFlagMetadata>,
-    @inject(AuthenticationBindings.CURRENT_USER)
-    private readonly user: IAuthUserWithPermissions | undefined,
-    @repository(FeatureRepository)
-    public featureRepository: FeatureRepository,
-    @repository(FeatureToggleRepository)
-    public featureToggleRepository: FeatureToggleRepository,
-    @repository(StrategyRepository)
-    public strategyRepository: StrategyRepository,
+    @inject('services.StrategyHelperService')
+    private readonly strategyHelperService: StrategyHelperService,
   ) {}
   value(): FeatureInterface {
     return () => this.isEnabled();
   }
 
   async isEnabled() {
-    const metadata: FeatureFlagMetadata = await this.getMetadata();
-    const featureName = metadata.feature;
-
-    const feature = await this.featureRepository.findOne({
-      where: {
-        name: featureName,
-      },
-    });
-    if (!feature) {
-      return false;
-    }
-
-    const featureToggle = await this.featureToggleRepository.findOne({
-      where: {
-        featureId: feature.id,
-        itemId: this.user?.tenantId,
-      },
-    });
-
-    //When toggle is not set
-    if (!featureToggle) {
-      const strategy = await this.strategyRepository.findOne({
-        where: {
-          name: 'Tenant',
-        },
-      });
-
-      await this.featureToggleRepository.create({
-        featureId: feature?.id,
-        strategyId: strategy?.id,
-        itemId: this.user?.tenantId,
-        status: true,
-      });
-
-      return true;
-    }
-
-    return featureToggle.status;
+    return this.strategyHelperService.isFeatureEnabled('Tenant');
   }
 }
