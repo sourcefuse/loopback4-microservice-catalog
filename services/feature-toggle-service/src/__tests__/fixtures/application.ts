3f7bdd2677bcfe87da7a251e2799197eb4a60b80
Ankur <ankur.bansal@sourcefuse.com>
2022-03-22 13:52:09 +0530

    feat(feature-toggle-service): removed unleash

--- services/feature-toggle-service/src/__tests__/fixtures/application.ts
+++ services/feature-toggle-service/src/__tests__/fixtures/application.ts
@@ -9,6 +9,13 @@ import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {FeatureToggleServiceComponent} from '../../component';
+import {
+  AuthorizationBindings,
+  AuthorizationComponent,
+} from 'loopback4-authorization';
+import {AuthenticationComponent, Strategies} from 'loopback4-authentication';
+import {BearerTokenVerifyProvider} from './bearer-token-verifier.provider';
+import {FeatureToggleMockDataSource} from './datasources/feature-toggle-mock.datasource';
 export {ApplicationConfig};
 require('dotenv').config();
 
@@ -27,6 +34,19 @@ export class TestingApplication extends BootMixin(
     });
     this.component(RestExplorerComponent);
     this.component(FeatureToggleServiceComponent);
+
+    this.component(AuthenticationComponent);
+    this.bind(Strategies.Passport.BEARER_TOKEN_VERIFIER).toProvider(
+      BearerTokenVerifyProvider,
+    );
+
+    // Add authorization component
+    this.bind(AuthorizationBindings.CONFIG).to({
+      allowAlwaysPaths: ['/explorer'],
+    });
+    this.component(AuthorizationComponent);
+
+    this.dataSource(FeatureToggleMockDataSource);
 
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
--- services/feature-toggle-service/src/__tests__/fixtures/test-helper.ts
+++ services/feature-toggle-service/src/__tests__/fixtures/test-helper.ts
@@ -3,10 +3,14 @@ import {
   givenHttpServerConfig,
   Client,
 } from '@loopback/testlab';
-import {StrategyBindings} from '../../keys';
+import {FeatureToggleDbName} from '../..';
+import {
+  FeatureRepository,
+  FeatureToggleRepository,
+  StrategyRepository,
+} from '../../repositories';
 import {TestingApplication} from './application';
-import {TestSystemStrategyProvider} from './system-strategy.provider';
-import {TestStrategyHelperService} from './test-strategy-helper.service';
+import {TestController} from './controllers/test.controller';
 
 export async function setupApplication(): Promise<AppWithClient> {
   const restConfig = givenHttpServerConfig({});
@@ -17,16 +21,86 @@ export async function setupApplication(): Promise<AppWithClient> {
 
   await app.boot();
 
-  app
-    .bind(StrategyBindings.SYSTEM_STRATEGY)
-    .toProvider(TestSystemStrategyProvider);
-  app.bind('test.StrategyHelperService').toClass(TestStrategyHelperService);
+  app.bind(`datasources.config.${FeatureToggleDbName}`).to({
+    name: FeatureToggleDbName,
+    connector: 'memory',
+  });
+
+  app.controller(TestController);
+
+  //Seed data
+  await seed(app);
 
   await app.start();
 
   const client = createRestAppClient(app);
 
   return {app, client};
+}
+
+async function seed(app: TestingApplication) {
+  const featureToggleRepo = await app.getRepository(FeatureToggleRepository);
+  const strategyRepository = await app.getRepository(StrategyRepository);
+  const featureRepo = await app.getRepository(FeatureRepository);
+
+  await featureRepo.createAll([
+    {
+      id: 'feature_1',
+      name: 'feature_1',
+      key: 'feature_1',
+    },
+    {
+      id: 'feature_2',
+      name: 'feature_2',
+      key: 'feature_2',
+    },
+    {
+      id: 'feature_3',
+      name: 'feature_3',
+      key: 'feature_3',
+    },
+    {
+      id: 'feature_4',
+      name: 'feature_4',
+      key: 'feature_4',
+    },
+  ]);
+
+  await strategyRepository.createAll([
+    {
+      name: 'System',
+      key: 'System',
+    },
+    {
+      name: 'Tenant',
+      key: 'Tenant',
+    },
+    {
+      name: 'User',
+      key: 'User',
+    },
+  ]);
+
+  await featureToggleRepo.createAll([
+    {
+      featureKey: 'feature_4',
+      strategyKey: 'System',
+      strategyEntityId: 'feature_4',
+      status: false,
+    },
+    {
+      featureKey: 'feature_3',
+      strategyKey: 'Tenant',
+      strategyEntityId: 'tenant_id',
+      status: false,
+    },
+    {
+      featureKey: 'feature_2',
+      strategyKey: 'User',
+      strategyEntityId: 'user_tenant_id',
+      status: false,
+    },
+  ]);
 }
 
 export interface AppWithClient {
