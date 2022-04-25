96cb609f8bcb14fa37dd06569d35c1c735d8b85f
Ankur Bansal <85155003+AnkurBansalSF@users.noreply.github.com>
2022-04-25 15:00:17 +0530

    fix(authentication-service): fix error messages in providers (#732)
    Fixed error messages in Sign-up providers
    
    gh-688

--- services/authentication-service/src/providers/keycloak-signup.provider.ts
+++ services/authentication-service/src/providers/keycloak-signup.provider.ts
@@ -1,14 +1,15 @@
 import {Provider} from '@loopback/context';
 import {HttpErrors} from '@loopback/rest';
 import {KeyCloakSignUpFn} from './types';
-import {AuthErrorKeys} from 'loopback4-authentication/index';
 
 export class KeyCloakSignupProvider implements Provider<KeyCloakSignUpFn> {
   constructor() {}
 
   value(): KeyCloakSignUpFn {
     return async profile => {
-      throw new HttpErrors.Unauthorized(AuthErrorKeys.InvalidCredentials);
+      throw new HttpErrors.NotImplemented(
+        `KeyCloakSignupProvider not implemented`,
+      );
     };
   }
 }
