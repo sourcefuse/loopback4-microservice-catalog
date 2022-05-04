ae39079a4d49f8943ee7692759b9a80df59f70e2
Barleen  Dhaliwal <barleen.dhaliwal@sourcefuse.com>
2022-05-04 18:09:16 +0530

    fix(user-onboarding): resolve dependency conflict on npm install with node v16
    gh-0

--- packages/user-onboarding/projects/user-onboarding-lib/src/test.ts
+++ packages/user-onboarding/projects/user-onboarding-lib/src/test.ts
@@ -1,14 +1,20 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing'; // NOSONAR
-import { getTestBed } from '@angular/core/testing';
+import 'zone.js';
+import 'zone.js/dist/zone-testing.js';
+
+import {getTestBed} from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing'; // NOSONAR
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp,
+  ): {
     keys(): string[];
     <T>(id: string): T;
   };
@@ -17,7 +23,7 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(),
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
