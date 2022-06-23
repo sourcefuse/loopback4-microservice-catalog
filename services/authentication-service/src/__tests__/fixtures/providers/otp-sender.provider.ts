00971f26b9cd8aaa2c9e887ffb508e3b9a8bbe4b
Ankur <ankur.bansal@sourcefuse.com>
2022-06-23 22:23:49 +0530

    test(authentication-service): sonar fix in tests

--- services/authentication-service/src/__tests__/fixtures/providers/otp-sender.provider.ts
+++ services/authentication-service/src/__tests__/fixtures/providers/otp-sender.provider.ts
@@ -3,8 +3,6 @@ import {User} from '../../../models';
 import {OtpSenderFn} from '../../../providers';
 
 export class OtpSenderProvider implements Provider<OtpSenderFn> {
-  constructor() {}
-
   value(): OtpSenderFn {
     return async (_otp: string, _user: User) => {
       // this is intentional
