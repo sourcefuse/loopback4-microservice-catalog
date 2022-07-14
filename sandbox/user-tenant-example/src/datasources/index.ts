deb184a7fc7e54ce59b37cd8c626e25ecc9fec5e
sumiter92 <46099054+sumiter92@users.noreply.github.com>
2022-07-14 16:31:51 +0530

    feat(sandbox): adds user-tenant-example (#913)

---
+++ sandbox/user-tenant-example/src/datasources/index.ts
@@ -0,0 +1,2 @@
+export * from './auth.datasource';
+export * from './cache.datasource';
---
+++ sandbox/user-tenant-example/src/index.ts
@@ -0,0 +1,40 @@
+import {ApplicationConfig, UserTenantApplication} from './application';
+
+export * from './application';
+
+const port = 3000;
+export async function main(options: ApplicationConfig = {}) {
+  const app = new UserTenantApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
+
+if (require.main === module) {
+  // Run the application
+  const config = {
+    rest: {
+      port: +(process.env.PORT ?? port),
+      host: process.env.HOST,
+      // The `gracePeriodForClose` provides a graceful close for http/https
+      // servers with keep-alive clients. The default value is `Infinity`
+      // (don't force-close). If you want to immediately destroy all sockets
+      // upon stop, set its value to `0`.
+      // See https://www.npmjs.com/package/stoppable
+      gracePeriodForClose: 5000, // 5 seconds
+      openApiSpec: {
+        // useful when used with OpenAPI-to-GraphQL to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
+}
