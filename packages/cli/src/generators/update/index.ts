1f12c84e2b9e512e082027769c3ebb3cb4018f18
Yesha  Mavani <yesha.mavani@sourcefuse.com>
2022-09-23 18:47:33 +0530

    fix(cli): sonar
    gh-1023

--- packages/cli/src/generators/update/index.ts
+++ packages/cli/src/generators/update/index.ts
@@ -50,7 +50,6 @@ export default class UpdateGenerator extends BaseUpdateGenerator<UpdateOptions> 
             ),
           );
 
-          if (this.shouldExit()) return;
           await this._updateSourceloopDep();
         }
       }
