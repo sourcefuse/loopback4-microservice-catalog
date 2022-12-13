f6fd32b7e0eb7f733955be63dfd2a4ba35cc18ae
Yesha  Mavani <yesha.mavani@sourcefuse.com>
2022-12-13 21:30:47 +0530

    refactor(chore): changes in the docker-compose
    MIGRATION CHANGE:
    migration-20221122072057- rectify migration
    migration-20221123082705- rectify migration
    
    gh-00

--- sandbox/pubnub-example/src/application.ts
+++ sandbox/pubnub-example/src/application.ts
@@ -18,8 +18,6 @@ import {
 } from '@sourceloop/notification-service';
 import {NotificationBindings} from 'loopback4-notifications';
 import {PubnubBindings, PubNubProvider} from 'loopback4-notifications/pubnub';
-import {SNSBindings} from 'loopback4-notifications/sns';
-import {SESBindings} from 'loopback4-notifications/ses';
 
 export {ApplicationConfig};
 
@@ -41,12 +39,12 @@ export class PubnubExampleApplication extends BootMixin(
     this.component(RestExplorerComponent);
     this.component(NotificationServiceComponent);
 
-    this.bind(NotifServiceBindings.Config).to({
-      useCustomEmailProvider: false,
-      useCustomSMSProvider: false,
-      useCustomPushProvider: true,
-      useCustomSequence: false,
-    });
+    // this.bind(NotifServiceBindings.Config).to({
+    //   useCustomEmailProvider: false,
+    //   useCustomSMSProvider: false,
+    //   useCustomPushProvider: true,
+    //   useCustomSequence: false,
+    // });
 
     /*
       subscribeKey: String - key used for subscribing to a channel (mandatory)
@@ -69,8 +67,6 @@ export class PubnubExampleApplication extends BootMixin(
       apns2BundleId: process.env.APP_BUNDLE_ID,
     });
 
-    this.bind(SNSBindings.Config).to({});
-    this.bind(SESBindings.Config).to({});
     this.bind(NotificationBindings.PushProvider).toProvider(PubNubProvider);
 
     this.projectRoot = __dirname;
