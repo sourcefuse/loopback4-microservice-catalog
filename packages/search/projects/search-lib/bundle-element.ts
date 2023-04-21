f1635bc8aaee07eab8cce61da39b40e8f4ce1356
yeshamavani <yesha.mavani@sourcefuse.com>
2023-04-21 14:59:15 +0530

    feat(search-client): publishing the search client as a web component
    gh-1386

--- packages/search/projects/search-lib/bundle-element.ts
+++ packages/search/projects/search-lib/bundle-element.ts
@@ -3,31 +3,21 @@ import {copyFile, rm} from 'fs/promises';
 const concat = require('concat');
 
 async function elementsBundler() {
-  const space = 2;
   const files = [
     './dist/search-element/runtime.js',
     './dist/search-element/polyfills.js',
     './dist/search-element/main.js',
   ];
-  await ensureDir('./dist/element');
-  await concat(files, './dist/element/search-element.js');
+  //copy the element related js and css file to separate project
+  //so that it can be published individually
+  //and then delete it from library
+  await ensureDir('../search-element');
+  await concat(files, '../search-element/search-element.js');
   await copyFile(
     './dist/search-element/styles.css',
-    './dist/element/styles.css',
+    '../search-element/styles.css',
   );
   await rm('./dist/search-element', {recursive: true});
-
-  //copy the package.json and change the name to
-  //so that element can be released separately with different name
-  //the versions will be same
-  const packageJsonPath = './package.json';
-  const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
-  jsonObj.name = '@sourceloop/search-element';
-  delete jsonObj.peerDependencies;
-  writeFileSync(
-    './dist/element/package.json',
-    JSON.stringify(jsonObj, null, space),
-  );
 }
 
 elementsBundler();
