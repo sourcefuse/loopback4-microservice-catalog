3c8ca093e9b2accc5e0c608516a009d246c427fe
Shubham P <shubham.prajapat@sourcefuse.com>
2023-04-26 09:18:02 +0530

    feat(video-conferencing-service): add support for configurable audit logs
    based on an env flag now users will be able to enable audit logs in the video conferencing service
    
    GH-1368

--- services/video-conferencing-service/src/repositories/index.ts
+++ services/video-conferencing-service/src/repositories/index.ts
@@ -3,5 +3,6 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 export * from './audit-logs.repository';
-export * from './video-chat-session.repository';
+export * from './audit.repository';
 export * from './session-attendees.repository';
+export * from './video-chat-session.repository';
