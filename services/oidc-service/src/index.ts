06e14e62f340d5cb8191e0025c41a3cd272646e8
antriksha102 <128211984+antriksha102@users.noreply.github.com>
2023-05-04 19:33:27 +0530

    sonar fixes (#1407)

--- services/oidc-service/src/index.ts
+++ services/oidc-service/src/index.ts
@@ -4,10 +4,6 @@
 // https://opensource.org/licenses/MIT
 import './load-env';
 export * from './component';
-// export * from './keys';
 export * from './models';
-// export * from './providers';
 export * from './repositories';
-// export * from './services';
 export * from './types';
-// export * from './enums';
