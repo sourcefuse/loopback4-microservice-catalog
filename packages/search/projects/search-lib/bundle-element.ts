bfcb6431b7312b1cfb2bea20ed8521a988a01d37
yeshamavani <yesha.mavani@sourcefuse.com>
2023-05-08 23:13:38 +0530

    feat(search-client): rebasing
    gh-1386

--- packages/search/projects/search-lib/bundle-element.ts
+++ packages/search/projects/search-lib/bundle-element.ts
@@ -1,8 +1,10 @@
-import {ensureDir} from 'fs-extra';
-import {copyFile, readFile, rm, writeFile} from 'fs/promises';
+import {ensureDir, readFileSync, writeFileSync} from 'fs-extra';
+import {copyFile, rm} from 'fs/promises';
+import * as crypto from 'crypto';
 const concat = require('concat');
 
 async function elementsBundler() {
+  const space = 2;
   const files = [
     './dist/search-element/runtime.js',
     './dist/search-element/polyfills.js',
@@ -11,34 +13,35 @@ async function elementsBundler() {
   //copy the element related js and css file to separate project
   //so that it can be published individually
   //and then delete it from library
-  await ensureDir('./dist/search-element-dist');
-  await concat(files, './dist/search-element-dist/search-element.js');
+  await ensureDir('../search-element');
+  await concat(files, '../search-element/search-element.js');
   await copyFile(
     './dist/search-element/styles.css',
-    './dist/search-element-dist/styles.css',
-  );
-  const packageJson = await readFile('./dist/search-lib/package.json', {
-    encoding: 'utf-8',
-  }).then(json => JSON.parse(json));
-  // delete packageJson.scripts;
-  delete packageJson.devDependencies;
-  delete packageJson.peerDependencies;
-  delete packageJson.dependencies;
-  delete packageJson.module;
-  delete packageJson.es2020;
-  delete packageJson.esm2020;
-  delete packageJson.fesm2020;
-  delete packageJson.fesm2015;
-  delete packageJson.typings;
-  delete packageJson.exports;
-  delete packageJson.sideEffects;
-  packageJson.main = './search-element.js';
-  packageJson.name = '@search/search-element';
-  await writeFile(
-    './dist/search-element-dist/package.json',
-    JSON.stringify(packageJson, null, 2),
+    '../search-element/styles.css',
   );
   await rm('./dist/search-element', {recursive: true});
+
+  // generate the hash of element file to keep a track of the chnages
+  // whenever any changes are made in the element the corresponding js file will
+  // resulting in changing its hash, so the element will also be published
+
+  const file = '../search-element/dist/search-element.js';
+
+  try {
+    const data = readFileSync(file);
+    const hash = crypto.createHash('sha256');
+    hash.update(data);
+
+    const fileHash = hash.digest('hex');
+    //update the package.json with the new hashCode
+    //if there will be changes in hash then the element will also be published
+    const packageJsonPath = '../search-element/package.json';
+    const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
+    jsonObj.hash = fileHash;
+    writeFileSync(packageJsonPath, JSON.stringify(jsonObj, null, space));
+  } catch (err: any) {
+    console.error(`An error occurred while reading ${file}: ${err.message}`);
+  }
 }
 
 elementsBundler();
