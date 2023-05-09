aafbd8be94e3548bbaea11afd2858f1270d00993
yeshamavani <83634146+yeshamavani@users.noreply.github.com>
2023-05-09 11:30:48 +0530

    feat(search-client): publishing the search client as a web component (#1387)
    * feat(search-client): publishing the search client as a web component
    
    gh-1386
    
    * fix(chore): update build process for search element
    
    GH-1386
    
    * feat(search-client): rebasing
    
    gh-1386
    
    * feat(search-client): rebasing
    
    gh-1386
    
    * feat(search-client): rebasing
    
    gh-1386
    
    * feat(search-client): sonar issues
    
    gh-1386
    
    ---------
    
    Co-authored-by: akshatdubeysf <akshat.dubey@sourcefuse.com>

--- packages/search/projects/search-lib/bundle-element.ts
+++ packages/search/projects/search-lib/bundle-element.ts
@@ -1,5 +1,6 @@
 import {ensureDir, readFileSync, writeFileSync} from 'fs-extra';
 import {copyFile, rm} from 'fs/promises';
+import * as crypto from 'crypto';
 const concat = require('concat');
 
 async function elementsBundler() {
@@ -9,25 +10,39 @@ async function elementsBundler() {
     './dist/search-element/polyfills.js',
     './dist/search-element/main.js',
   ];
-  await ensureDir('./dist/element');
-  await concat(files, './dist/element/search-element.js');
+
+  await ensureDir('../search-element/dist');
+  await concat(files, '../search-element/dist/search-element.js');
   await copyFile(
     './dist/search-element/styles.css',
-    './dist/element/styles.css',
+    '../search-element/dist/styles.css',
   );
+  await copyFile('./dist/README.md', '../search-element/dist/README.md');
   await rm('./dist/search-element', {recursive: true});
 
-  //copy the package.json and change the name to
-  //so that element can be released separately with different name
-  //the versions will be same
-  const packageJsonPath = './package.json';
-  const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
-  jsonObj.name = '@sourceloop/search-element';
-  delete jsonObj.peerDependencies;
-  writeFileSync(
-    './dist/element/package.json',
-    JSON.stringify(jsonObj, null, space),
-  );
+  // generate the hash of element file to keep a track of the chnages
+  // whenever any changes are made in the element the corresponding js file will
+  // resulting in changing its hash, so the element will also be published
+
+  const file = '../search-element/dist/search-element.js';
+  try {
+    const data = readFileSync(file);
+    const hash = crypto.createHash('sha256');
+    hash.update(data);
+
+    const fileHash = hash.digest('hex');
+
+    //update the package.json with the new hashCode
+    //if there will be changes in hash then the element will also be published
+    const packageJsonPath = '../search-element/package.json';
+    const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
+    jsonObj.hash = fileHash;
+    writeFileSync(packageJsonPath, JSON.stringify(jsonObj, null, space));
+    // sonarignore:start
+  } catch (err: any) {
+    console.error(`An error occurred while reading ${file}: ${err.message}`);
+    // sonarignore:end
+  }
 }
 
 elementsBundler();
