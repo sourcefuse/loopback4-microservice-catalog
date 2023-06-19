482e7a929140aa79d541a5e8c884963136aa41c6
Shubham P <shubham.prajapat@sourcefuse.com>
2023-06-19 15:55:22 +0530

    chore(ocr-service): remove ocr-service and ocr-s3-service (#1454)
    as they are no longer maintained and unpublished from npm

--- services/ocr-service/src/models/index.ts
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) 2023 Sourcefuse Technologies
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-export * from './contracts.model';
-export * from './ocr-results.model';
-export * from './hocr-results.model';
