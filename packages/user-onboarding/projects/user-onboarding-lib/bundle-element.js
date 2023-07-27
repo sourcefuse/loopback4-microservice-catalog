a4292b2b56cc27614a4a0a47cfde356829cd71b1
Yesha Mavani <yesha.mavani@sourcefuse.com>
2023-07-27 14:55:07 +0530

    feat(user-onboarding): adding changes to publish user onboarding lib are element
    gh-1352

--- packages/user-onboarding/projects/user-onboarding-lib/bundle-element.js
+++ packages/user-onboarding/projects/user-onboarding-lib/bundle-element.js
@@ -1,6 +1,7 @@
 const concat = require('concat');
-const {rm} = require('fs/promises');
+const {rm, copyFile} = require('fs/promises');
 const {ensureDir, readFileSync, writeFileSync} = require('fs-extra');
+const crypto = require('crypto');
 
 async function elementsBundler() {
   const space = 2;
@@ -9,20 +10,37 @@ async function elementsBundler() {
     './dist/user-onboarding-element/polyfills.js',
     './dist/user-onboarding-element/main.js',
   ];
-  await ensureDir('./dist/element');
-  await concat(files, './dist/element/user-onboarding-element.js');
+  //await ensureDir('./dist/element');
+  await ensureDir('../user-onboarding-element/dist');
+  await concat(
+    files,
+    '../user-onboarding-element/dist/user-onboarding-element.js',
+  );
+
+  await copyFile(
+    './dist/README.md',
+    '../user-onboarding-element/dist/README.md',
+  );
 
   await rm('./dist/user-onboarding-element', {recursive: true});
 
-  //copy the package.json and change the name to element
-  const packageJsonPath = './package.json';
+  // generate the hash of element file to keep a track of the chnages
+  // whenever any changes are made in the element the corresponding js file will
+  // resulting in changing its hash, so the element will also be published
+
+  const file = '../user-onboarding-element/dist/user-onboarding-element.js';
+  const data = readFileSync(file);
+  const hash = crypto.createHash('sha256');
+  hash.update(data);
+
+  const fileHash = hash.digest('hex');
+
+  //update the package.json with the new hashCode
+  //if there will be changes in hash then the element will also be published
+  const packageJsonPath = '../user-onboarding-element/package.json';
   const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
-  jsonObj.name = '@sourceloop/user-onboarding-element';
-  delete jsonObj.peerDependencies;
-  writeFileSync(
-    './dist/element/package.json',
-    JSON.stringify(jsonObj, null, space),
-  );
+  jsonObj.hash = fileHash;
+  writeFileSync(packageJsonPath, JSON.stringify(jsonObj, null, space));
 }
 
 elementsBundler();
