eebbe4ec42957350bca5ad2883c49a15afedbd83
RaghavaroraSF <97958393+RaghavaroraSF@users.noreply.github.com>
2023-07-28 16:32:18 +0530

    feat(user-onboarding): add element support and script for building (#1377)
    * feat(user-onboarding): rebasing
    
    gh-1352
    
    * feat(user-onboarding): rebasing
    
    gh-1352
    
    * feat(user-onboarding): rebasing
    
    gh-1352
    
    * feat(user-onboarding): rebasing 
    
    gh-1352
    
    * feat(user-onboarding): rebasing 
    
    #1352
    
    gh-1352
    
    * feat(user-onboarding): adding changes to publish user onboarding lib are element
    
    gh-1352
    
    * feat(user-onboarding): changes to publish user onboarding web element
    
    gh-1352
    
    * feat(user-onboarding): sonar fix
    
    gh-1352
    
    ---------
    
    Co-authored-by: Yesha Mavani <yesha.mavani@sourcefuse.com>
    Co-authored-by: Deepika516 <deepika.mahindroo@sourcefuse.com>

--- packages/user-onboarding/projects/user-onboarding-lib/bundle-element.js
+++ packages/user-onboarding/projects/user-onboarding-lib/bundle-element.js
@@ -1,30 +1,33 @@
-import * as crypto from 'crypto';
-import {ensureDir, readFileSync, writeFileSync} from 'fs-extra';
-import {copyFile, rm} from 'fs/promises';
 const concat = require('concat');
+const {rm, copyFile} = require('fs/promises');
+const {ensureDir, readFileSync, writeFileSync} = require('fs-extra');
+const crypto = require('crypto');
 
 async function elementsBundler() {
   const space = 2;
   const files = [
-    './dist/search-element/runtime.js',
-    './dist/search-element/polyfills.js',
-    './dist/search-element/main.js',
+    './dist/user-onboarding-element/runtime.js',
+    './dist/user-onboarding-element/polyfills.js',
+    './dist/user-onboarding-element/main.js',
   ];
-
-  await ensureDir('../search-element/dist');
-  await concat(files, '../search-element/dist/search-element.js');
-  await copyFile(
-    './dist/search-element/styles.css',
-    '../search-element/dist/styles.css',
+  await ensureDir('../user-onboarding-element/dist');
+  await concat(
+    files,
+    '../user-onboarding-element/dist/user-onboarding-element.js',
   );
-  await copyFile('./dist/README.md', '../search-element/dist/README.md');
-  await rm('./dist/search-element', {recursive: true});
+
+  await copyFile(
+    './dist/README.md',
+    '../user-onboarding-element/dist/README.md',
+  );
+
+  await rm('./dist/user-onboarding-element', {recursive: true});
 
   // generate the hash of element file to keep a track of the chnages
   // whenever any changes are made in the element the corresponding js file will
   // resulting in changing its hash, so the element will also be published
 
-  const file = '../search-element/dist/search-element.js';
+  const file = '../user-onboarding-element/dist/user-onboarding-element.js';
   const data = readFileSync(file);
   const hash = crypto.createHash('sha256');
   hash.update(data);
@@ -33,7 +36,7 @@ async function elementsBundler() {
 
   //update the package.json with the new hashCode
   //if there will be changes in hash then the element will also be published
-  const packageJsonPath = '../search-element/package.json';
+  const packageJsonPath = '../user-onboarding-element/package.json';
   const jsonObj = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
   jsonObj.hash = fileHash;
   writeFileSync(packageJsonPath, JSON.stringify(jsonObj, null, space));
---
+++ packages/user-onboarding/projects/user-onboarding-lib/src/lib/user-onboarding-element.module.ts
@@ -0,0 +1,38 @@
+// Copyright (c) 2022 Sourcefuse Technologies
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import {NgModule, Injector} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {TourServiceService} from './tour-service.service';
+import {TourStoreServiceService} from './tour-store-service.service';
+
+@NgModule({
+  imports: [
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
+    BrowserModule,
+  ],
+  bootstrap: [],
+})
+export class UserOnboardingElementModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const tourservice = this.injector.get(TourServiceService);
+    const tourstoreservice = this.injector.get(TourStoreServiceService);
+    
+
+    // to export the service for vanilla JS projects
+    Object.assign(window, {
+      tourservice,
+      tourstoreservice,
+    });
+  }
+}
