1501dad82947f55eaa04696430bfb34a2c99d748
Tyagi-Sunny <sunny.tyagi@sourcefuse.com>
2023-08-16 16:55:40 +0530

    fix(sandbox): deploy service's dependencies using ecr images
    deploy service's dependencies using ecr images

--- sandbox/telemed-app/backend/video-conferencing-service/scripts/build-dependency-layer.sh
+++ sandbox/telemed-app/backend/video-conferencing-service/scripts/build-dependency-layer.sh
@@ -9,8 +9,6 @@ LAYER_DIR="$ROOT_DIR/layers/nodejs"
 
 rm -rf $LAYER_DIR
 mkdir -p "$LAYER_DIR"
-cp "loopback4-authentication-9.0.1.tgz" "$LAYER_DIR"
-cp "sourceloop-video-conferencing-service-5.0.3.tgz" "$LAYER_DIR"
 cp "package.json" "$LAYER_DIR"
 cd "$LAYER_DIR"
 npm i --omit=dev
