82432e668b7ee1c8cbaa0b4dc6e9619ab39f9ca1
Yesha Mavani <yesha.mavani@sourcefuse.com>
2023-10-05 20:46:26 +0530

    refactor(all-services): remove the search and user onboarding libraries from catalog
    gh-00

--- packages/user-onboarding/projects/user-onboarding-lib/src/models/filter-function.ts
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) 2023 Sourcefuse Technologies
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-import {TourStep} from './tour-step';
-
-export type FilterFunction = (steps: TourStep[]) => TourStep[];
