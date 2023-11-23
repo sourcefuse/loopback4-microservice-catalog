e841cbcad27f7f4f9874f0c21a69414108983ad4
Surbhi-sharma1 <surbhi.sharma1@sourcefuse.com>
2023-11-23 13:20:43 +0530

    feat(payment-service): add sequelize support in payment service
    add sequelize support in payment service
    
    GH-1350

---
+++ services/payment-service/src/repositories/sequelize/subscriptions.repository.ts
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2023 Sourcefuse Technologies
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+import {inject} from '@loopback/core';
+import {
+  SequelizeCrudRepository,
+  SequelizeDataSource,
+} from '@loopback/sequelize';
+import {PaymentDatasourceName} from '../../keys';
+import {Subscriptions} from '../../models';
+
+export class SubscriptionsRepository extends SequelizeCrudRepository<
+  Subscriptions,
+  typeof Subscriptions.prototype.id
+> {
+  constructor(
+    @inject(`datasources.${PaymentDatasourceName}`)
+    dataSource: SequelizeDataSource,
+  ) {
+    super(Subscriptions, dataSource);
+  }
+}
