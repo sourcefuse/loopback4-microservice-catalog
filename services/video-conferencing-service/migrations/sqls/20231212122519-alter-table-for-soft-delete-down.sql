d9959c8de4b853607761b1bcd542c37770dad6d2
Karunesh Mani Tripathi <karunesh@sourcefuse.com>
2023-12-13 02:19:06 +0530

    feat(chore): added migrations
    added migrations
    
    GH-1821

---
+++ services/video-conferencing-service/migrations/sqls/20231212122519-alter-table-for-soft-delete-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+ALTER TABLE videochatms.video_session_details DROP COLUMN IF EXISTS deleted_by;
+ALTER TABLE videochatms.video_session_details DROP COLUMN IF EXISTS deleted_on;
