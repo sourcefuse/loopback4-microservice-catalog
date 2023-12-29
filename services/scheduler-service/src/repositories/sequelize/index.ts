c36a2e33fac491380c6e95a9db576ca7af685d72
Surbhi <98279679+Surbhi-sharma1@users.noreply.github.com>
2023-12-29 21:41:15 +0530

    feat(scheduler-service): add sequelize support in scheduler service (#1871)
    feat(scheduler-service): add sequelize as optional dependency
    
    add sequelize as optional dependency
    
    GH-1350

---
+++ services/scheduler-service/src/repositories/sequelize/index.ts
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2023 Sourcefuse Technologies
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+export * from './attachment.repository';
+export * from './attendee.repository';
+export * from './audit.repository';
+export * from './calendar.repository';
+export * from './event-attendee-view.repository';
+export * from './event.repository';
+export * from './settings.repository';
+export * from './subscription.repository';
+export * from './theme.repository';
+export * from './working-hour.repository';
---
+++ services/scheduler-service/src/repositories/sequelize/working-hour.repository.ts
@@ -0,0 +1,57 @@
+﻿// Copyright (c) 2023 Sourcefuse Technologies
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+import {Getter, inject} from '@loopback/core';
+import {BelongsToAccessor, repository} from '@loopback/repository';
+import {SequelizeDataSource} from '@loopback/sequelize';
+import {
+  ConditionalAuditRepositoryMixin,
+  IAuditMixinOptions,
+} from '@sourceloop/audit-log';
+import {IAuthUserWithPermissions} from '@sourceloop/core';
+import {SequelizeUserModifyCrudRepository} from '@sourceloop/core/sequelize';
+import {AuthenticationBindings} from 'loopback4-authentication';
+import {SchedulerDatasourceName} from '../../keys';
+import {Calendar, WorkingHour, WorkingHourRelations} from '../../models';
+import {AuditLogRepository} from './audit.repository';
+import {CalendarRepository} from './calendar.repository';
+const WorkingHourAuditOpts: IAuditMixinOptions = {
+  actionKey: 'Working_Hours_Logs',
+};
+
+export class WorkingHourRepository extends ConditionalAuditRepositoryMixin(
+  SequelizeUserModifyCrudRepository<
+    WorkingHour,
+    typeof WorkingHour.prototype.id,
+    WorkingHourRelations
+  >,
+  WorkingHourAuditOpts,
+) {
+  public readonly calendar: BelongsToAccessor<
+    Calendar,
+    typeof WorkingHour.prototype.id
+  >;
+
+  constructor(
+    @inject(`datasources.${SchedulerDatasourceName}`)
+    dataSource: SequelizeDataSource,
+    @inject.getter(AuthenticationBindings.CURRENT_USER)
+    protected readonly getCurrentUser: Getter<
+      IAuthUserWithPermissions | undefined
+    >,
+    @repository.getter('CalendarRepository')
+    protected calendarRepositoryGetter: Getter<CalendarRepository>,
+    @repository.getter('AuditLogRepository')
+    public getAuditLogRepository: Getter<AuditLogRepository>,
+  ) {
+    super(WorkingHour, dataSource, getCurrentUser);
+
+    this.calendar = this.createBelongsToAccessorFor(
+      'calendar',
+      calendarRepositoryGetter,
+    );
+
+    this.registerInclusionResolver('calendar', this.calendar.inclusionResolver);
+  }
+}
