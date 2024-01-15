6d8d25a5ba5324c8b4eb084b06c0f2394e5fb3b4
akshatdubeysf <akshat.dubey@sourcefuse.com>
2024-01-15 10:40:00 +0530

    feat(task-service): rename sub task to user task interface
    BB-1785

--- services/task-service/src/interfaces/index.ts
+++ services/task-service/src/interfaces/index.ts
@@ -4,4 +4,4 @@ export * from './i-event-adapter';
 export * from './i-event-processor';
 export * from './i-incoming-connector';
 export * from './i-outgoing-connector';
-export * from './i-sub-task-service';
+export * from './i-user-task-service';
