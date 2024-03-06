9356678363cde433c97b514b8856fee93d24eb54
yeshamavani <83634146+yeshamavani@users.noreply.github.com>
2024-03-06 11:44:25 +0530

    feat(sandbox): added example for user tenant service (#2024)
    * chore(all-services): rebase
    
    MIGRATION CHANGE:
    migration-20230905130235- rebase
    
    gh-1954
    
    * feat(sandbox): add user tenant service example
    
    gh-1954
    
    * feat(sandbox): user tenant example
    
    MIGRATION CHANGE:
    migration-20230905130235- migration
    
    gh-1954
    
    * feat(sandbox): user tenant example
    
    gh-1954
    
    ---------
    
    Co-authored-by: Karunesh Mani Tripathi <karunesh@sourcefuse.com>

---
+++ sandbox/user-tenant-example/src/openapi-spec.ts
@@ -0,0 +1,31 @@
+import {ApplicationConfig} from '@loopback/core';
+import {UserTenantExampleApplication} from './application';
+
+const ARGV_INDEX = 2;
+const DEFAULT_PORT = 3000;
+/**
+ * Export the OpenAPI spec from the application
+ */
+async function exportOpenApiSpec(): Promise<void> {
+  const config: ApplicationConfig = {
+    rest: {
+      port: +(process.env.PORT ?? DEFAULT_PORT),
+      host: process.env.HOST ?? 'localhost',
+    },
+  };
+  const outFile = process.argv[ARGV_INDEX] ?? './src/openapi.json';
+  const app = new UserTenantExampleApplication(config);
+  await app.boot();
+  await app.exportOpenApiSpec(outFile);
+}
+
+exportOpenApiSpec()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch(err => {
+    // sonarignore:start
+    console.error('Fail to export OpenAPI spec from the application.', err);
+    // sonarignore:end
+    process.exit(1);
+  });
