72002ac6843494b119866d3929f2a1a1dc1beae7
karuneshsf <karuneshsf@users.noreply.github.com>
2024-03-14 12:29:45 +0530

    feat(authentication-service): added SAML example (#1912)
    * chore(chore): rebasing with master
    
    rebasing with master
    
    * chore(chore): rebasing with master
    
    rebasing with master
    
    * chore(chore): rebasing with master
    
    rebasing with master
    
    * chore(chore): rebasing with master
    
    rebasing with master
    
    * chore(chore): rebasing with master
    
    rebasing with master
    
    * chore(chore): rebasing with master
    
    rebasing with master
    
    * chore(chore): rebasing with master
    
    rebasing with master
    
    * feat(sandbox): code review changes
    
    code review changes
    
    BREAKING CHANGE:
    GH-1663
    
    * feat(sandbox): changes for test and cli build issue
    
    changes for test and cli build issue
    
    GH-1663
    
    ---------
    
    Co-authored-by: Karunesh Mani Tripathi <karunesh@sourcefuse.com>

--- sandbox/oauth-example/src/application.ts
+++ sandbox/oauth-example/src/application.ts
@@ -15,12 +15,18 @@ import {
   AuthenticationServiceComponent,
   SignUpBindings,
 } from '@sourceloop/authentication-service';
+import {Strategies} from 'loopback4-authentication';
+import {SamlStrategyFactoryProvider} from 'loopback4-authentication/SAML';
+import {GoogleAuthStrategyFactoryProvider} from 'loopback4-authentication/passport-google-oauth2';
+import {LocalPasswordStrategyFactoryProvider} from 'loopback4-authentication/passport-local';
 import path from 'path';
 import {
   AzureAdSignupProvider,
   FacebookOauth2SignupProvider,
   GoogleOauth2SignupProvider,
+  SamlVerifyProvider,
 } from './providers';
+import {SamlSignupProvider} from './providers/saml-signup.provider';
 import {MySequence} from './sequence';
 
 export {ApplicationConfig};
@@ -45,14 +51,31 @@ export class AuthServiceApplication extends BootMixin(
 
     this.component(AuthenticationServiceComponent);
 
+    this.bind(Strategies.Passport.LOCAL_STRATEGY_FACTORY.key).toProvider(
+      LocalPasswordStrategyFactoryProvider,
+    );
+
     this.bind(SignUpBindings.GOOGLE_SIGN_UP_PROVIDER).toProvider(
       GoogleOauth2SignupProvider,
     );
+    this.bind(
+      Strategies.Passport.GOOGLE_OAUTH2_STRATEGY_FACTORY.key,
+    ).toProvider(GoogleAuthStrategyFactoryProvider);
     this.bind(SignUpBindings.FACEBOOK_SIGN_UP_PROVIDER).toProvider(
       FacebookOauth2SignupProvider,
     );
     this.bind(SignUpBindings.AZURE_AD_SIGN_UP_PROVIDER).toProvider(
       AzureAdSignupProvider,
+    );
+
+    this.bind(SignUpBindings.SAML_SIGN_UP_PROVIDER).toProvider(
+      SamlSignupProvider,
+    );
+
+    this.bind(Strategies.Passport.SAML_VERIFIER).toProvider(SamlVerifyProvider);
+
+    this.bind(Strategies.Passport.SAML_STRATEGY_FACTORY.key).toProvider(
+      SamlStrategyFactoryProvider,
     );
 
     this.projectRoot = __dirname;
