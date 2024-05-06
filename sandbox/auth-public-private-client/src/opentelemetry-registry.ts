259eba8f47fd47378f37b4698cbe72f17d8de107
Sunny Tyagi <107617248+Tyagi-Sunny@users.noreply.github.com>
2024-05-06 20:03:26 +0530

    feat(authentication-service): add example for public private client i… (#2084)
    * feat(authentication-service): auth service example- public private client
    
    auth service example- public private client
    
    MIGRATION CHANGE:
    migration-20210318100600- auth service example- public private client
    migration-20220607063927- auth service example- public private client
    migration-20221110095658- auth service example- public private client
    migration-20230323132703- auth service example- public private client
    migration-20230524115047- auth service example- public private client
    migration-20240116123737- auth service example- public private client
    migration-20240502140840- auth service example- public private client
    
    gh-2080
    
    * chore(ci-cd): make it a private package to avoid publishing
    
    gh-00
    
    ---------
    
    Co-authored-by: yeshamavani <83634146+yeshamavani@users.noreply.github.com>
    Co-authored-by: Yesha Mavani <yesha.mavani@sourcefuse.com>

---
+++ sandbox/auth-public-private-client/src/opentelemetry-registry.ts
@@ -0,0 +1,35 @@
+import {JaegerExporter} from '@opentelemetry/exporter-jaeger';
+import {NodeTracerProvider} from '@opentelemetry/sdk-trace-node';
+import {
+  BatchSpanProcessor,
+  ConsoleSpanExporter,
+} from '@opentelemetry/sdk-trace-base';
+import * as dotenv from 'dotenv';
+import * as dotenvExt from 'dotenv-extended';
+
+dotenv.config();
+dotenvExt.load({
+  schema: '.env.example',
+  errorOnMissing: true,
+  includeProcessEnv: true,
+});
+
+if (!!+(process.env.ENABLE_TRACING ?? 0)) {
+  const provider = new NodeTracerProvider();
+  const option = {
+    serviceName: process.env.SERVICE_NAME ?? '',
+    tags: [],
+    // You can use the default UDPSender
+    host: process.env.OPENTELEMETRY_HOST ?? '',
+    port: process.env.OPENTELEMETRY_PORT
+      ? +process.env.OPENTELEMETRY_PORT
+      : 0,
+  };
+  // Configure span processor to send spans to the exporter
+  const exporter = new JaegerExporter(option);
+  provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(new ConsoleSpanExporter()),
+  );
+  provider.register();
+}
