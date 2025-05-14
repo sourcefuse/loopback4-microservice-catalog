606c6e70e99cfb1c0adec6a0173f605f2edfc8ab
vaibhav bhalla <vaibhav.bhalla@sourcefuse.com>
2025-05-14 15:08:47 +0530

    fix(all-services): sonar issue
    GH-2252

--- packages/custom-sf-changelog/parser-opts.js
+++ packages/custom-sf-changelog/parser-opts.js
@@ -5,6 +5,6 @@ module.exports = {
   headerCorrespondence: ['type', 'scope', 'subject'],
   noteKeywords: ['BREAKING CHANGE', 'MIGRATION CHANGE'],
   revertPattern:
-    /^(?:Revert|revert:)\s"([^"\r\n]{1,1000})"\s+This reverts commit ([a-fA-F0-9]{7,40})\.$/i,
+    /^(?:Revert|revert:)\s"([^"\r\n]{1,1000})"\s+This reverts commit ([a-f0-9]{7,40})\.$/i,
   revertCorrespondence: ['header', 'hash'],
 };
