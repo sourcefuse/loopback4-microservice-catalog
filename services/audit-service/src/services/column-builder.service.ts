4f8e90f4f91534df17efa1a63aa5fe49c8ffaf49
Vaibhav  Bhalla <vaibhav.bhalla@SFSupports-MacBook-Air.local>
2025-09-12 10:03:28 +0530

    refactor(chore): version upgrade
    BREAKING CHANGE:
    YES
    
    GH-2323-version-upgrade

--- services/audit-service/src/services/column-builder.service.ts
+++ services/audit-service/src/services/column-builder.service.ts
@@ -8,6 +8,13 @@ export class ColumnBuilderProvider implements Provider<ColumnBuilderFn> {
   value(): ColumnBuilderFn {
     return async (auditLogs: AuditLog[]) => this.columnBuilder(auditLogs);
   }
+  
+  /**
+   * The columnBuilder function in TypeScript returns an array of AuditLog objects asynchronously.
+   * @param {AuditLog[]} auditLogs - An array of AuditLog objects.
+   * @returns The `columnBuilder` function is returning a Promise that resolves to an array of
+   * `AuditLog` objects.
+   */
   async columnBuilder(auditLogs: AuditLog[]): Promise<AnyObject[]> {
     return auditLogs;
   }
