ffbac293ad2e227ba2186bac999fa6e6522572f9
Subham Jaguri <subham.jaguri@sourcefuse.com>
2025-10-13 18:53:11 +0530

    refactor(authentication-service): remove unused description from state property in ClientAuthRequest DTO
    - Eliminated the description for the state property in the ClientAuthRequest DTO to streamline the model and enhance clarity.

--- services/authentication-service/src/modules/auth/models/client-auth-request.dto.ts
+++ services/authentication-service/src/modules/auth/models/client-auth-request.dto.ts
@@ -28,7 +28,6 @@ export class ClientAuthRequest extends CoreModel<ClientAuthRequest> {
 
   @property({
     type: 'string',
-    description: ModelPropertyDescriptionString.reqStrPropDesc,
   })
   state?: string;
 }
