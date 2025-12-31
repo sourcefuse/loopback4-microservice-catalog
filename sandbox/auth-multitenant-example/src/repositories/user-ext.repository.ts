23fe65f4115831f91cb33ca329111564e3e8b67d
Vaibhav Bhalla <vaibhav.bhalla@sourcefuse.com>
2025-12-31 16:10:04 +0530

    refactor(all-services): fix sonarqube issues
    fix sonarqube issues

--- sandbox/auth-multitenant-example/src/repositories/user-ext.repository.ts
+++ sandbox/auth-multitenant-example/src/repositories/user-ext.repository.ts
@@ -129,6 +129,7 @@ export class UserExtRepository extends DefaultSoftCrudRepository<
       }
       await this.credentials(user.id).create(creds, options);
     } catch (err) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
       super.deleteByIdHard(user.id);
       throw new HttpErrors.UnprocessableEntity(
         `Error while hashing password ${err.message}`,
